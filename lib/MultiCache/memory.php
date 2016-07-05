f2c45079877ff70629813b9f98319d8c09a538e1
tom2tom <tpgww@onepost.net>
2016-07-05 12:13:36 +1000

    revert deleted cache-type

---
+++ lib/MultiCache/memory.php
@@ -0,0 +1,63 @@
+<?php
+namespace MultiCache;
+
+class Cache_memory extends CacheBase implements CacheInterface {
+
+	function __construct($config = array())	{
+	}
+
+/*	function __destruct() {
+		$this->driver_clean();
+	}
+*/
+	function checkdriver() {
+		return TRUE;
+	}
+		 
+	function driver_set($keyword, $value = '', $time = 300, $option = array()) {
+		if(empty($option['skipExisting']) ||
+			!array_key_exists($keyword, $this->index)) {
+			$this->index[$keyword] = $value;
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	function driver_get($keyword, $option = array()) {
+		if(array_key_exists($keyword, $this->index)) {
+			return $this->index[$keyword];
+		}
+		return NULL;
+	}
+
+	function driver_getall($option = array()) {
+		return array_keys($this->index);
+	}
+
+	function driver_delete($keyword, $option = array()) {
+		if(array_key_exists($keyword, $this->index)) {
+			unset($this->index[$keyword]);
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	function driver_stats($option = array()) {
+		return array(
+			'info' => 'Number of cached items',
+			'size' => count($this->index),
+			'data' => ''
+		);
+	}
+
+	function driver_clean($option = array()) {
+		$this->index = array();
+	}
+
+	function driver_isExisting($keyword) {
+		return array_key_exists($keyword, $this->index);
+	}
+
+}
+
+?>
