6ea96c2591da63385dbadc1bbcfabc9ba67ac65b
tom2tom <tpgww@onepost.net>
2016-07-10 19:01:06 +1000

    PSR-2 formatting support cache-namespaces

--- lib/Cache.php
+++ lib/Cache.php
@@ -12,89 +12,83 @@ namespace Booker;
 class Cache
 {
 	private static $cache = NULL; //intra-request cache-cache object
+
 	/**
-	GetCache:
-    @mod: reference to Booker-module object
-	@storage: optional cache-type name, one (or more, ','-separated) of
-	 auto,yac,apc,apcu,wincache,xcache,redis,predis,file,database
-	 default = 'auto' to try all of the above, in that order
-	@settings: optional array of general and cache-type-specific parameters,
-	 (e.g. see default array in this func)
-	 default empty
-	Returns: cache-object (after creating it if not already done) or NULL
-	*/
-	public function GetCache(&$mod,$storage='auto',$settings=array())
+	  GetCache:
+	  @mod: reference to Booker-module object
+	  @storage: optional cache-type name, one (or more, ','-separated) of
+	  auto,yac,apc,apcu,wincache,xcache,redis,predis,file,database
+	  default = 'auto' to try all of the above, in that order
+	  @settings: optional array of general and cache-type-specific parameters,
+	  (e.g. see default array in this func)
+	  default empty
+	  Returns: cache-object (after creating it if not already done) or NULL
+	 */
+	public function GetCache(&$mod, $storage = 'auto', $settings = array())
 	{
-//		if(self::$cache == NULL && isset($_SESSION['bkrcache']))
+//		if (self::$cache == NULL && isset($_SESSION['bkrcache']))
 //			self::$cache = $_SESSION['bkrcache'];
-		if(self::$cache)
+		if (self::$cache)
 			return self::$cache;
 
-		$path = __DIR__.DIRECTORY_SEPARATOR.'MultiCache'.DIRECTORY_SEPARATOR;
-		require($path.'CacheInterface.php');
-		require($path.'CacheBase.php');
+		$path = __DIR__ . DIRECTORY_SEPARATOR . 'MultiCache' . DIRECTORY_SEPARATOR;
+		require($path . 'CacheInterface.php');
+		require($path . 'CacheBase.php');
 
 		$config = cmsms()->GetConfig();
 		$url = (empty($_SERVER['HTTPS'])) ? $config['root_url'] : $config['ssl_url'];
 
 		$basedir = $config['uploads_path'];
-		if(is_dir($basedir))
-		{
+		if (is_dir($basedir)) {
 			$rel = $mod->GetPreference('pref_uploadsdir');
-			if($rel)
-			{
-				$basedir .= DIRECTORY_SEPARATOR.$rel;
+			if ($rel) 	{
+				$basedir .= DIRECTORY_SEPARATOR . $rel;
 			}
-		}
-		else
+		} else
 			$basedir = '';
 
 		$settings = array_merge(
 			array(
-/*				'memcache' => array(
-					array('host'=>$url,'port'=>11211)
-				),
-				'memcached' => array(
-					array('host'=>$url,'port'=>11211,'persist'=>1)
-				),
+/* 			'memcache' => array(
+			  array('host'=>$url,'port'=>11211)
+			  ),
+			  'memcached' => array(
+			  array('host'=>$url,'port'=>11211,'persist'=>1)
+			  ),
 */
-				'redis' => array(
-					'host' => $url //TODO CHECKME
-				),
-				'predis' => array(
-					'host' => $url
-				),
-				'file' => array(
-					'path' => $basedir
-				),
-				'database' => array(
-					'table' => cms_db_prefix().'module_bkr_cache'
-				)
+			'redis' => array(
+				'host' => $url //TODO CHECKME
+			),
+			'predis' => array(
+				'host' => $url
+			),
+			'file' => array(
+				'path' => $basedir
+			),
+			'database' => array(
+				'table' => cms_db_prefix() . 'module_bkr_cache'
+			)
 			), $settings);
 
-		if($storage)
-		{
+		if ($storage) {
 			$storage = strtolower($storage);
-		}
-		else
+		} else
 			$storage = 'auto';
-		if(strpos($storage,'auto') !== FALSE)
+		if (strpos($storage, 'auto') !== FALSE)
 			$storage = 'yac,apc,apcu,wincache,xcache,redis,predis,file,database';
 
-		$types = explode(',',$storage);
-		foreach($types as $one)
-		{
+		$types = explode(',', $storage);
+		foreach ($types as $one) {
 			$one = trim($one);
-			$class = 'MultiCache\Cache_'.$one;
-			if(!isset($settings[$one]))
-				$settings[$one] = [];
-			try
-			{
-				require($path.$one.'.php');
+			if (!isset($settings[$one]))
+				$settings[$one] = array();
+			if (empty($settings[$one]['namespace']))
+				$settings[$one]['namespace'] = $mod->GetName();
+			$class = 'MultiCache\Cache_' . $one;
+			try {
+				require($path . $one . '.php');
 				$cache = new $class($settings[$one]);
-			}
-			catch(\Exception $e)
-			{
+			} catch (\Exception $e) {
 				continue;
 			}
 //			$_SESSION['bkrcache'] = $cache;
@@ -112,4 +106,3 @@ class Cache
 	}
 
 }
-?>
