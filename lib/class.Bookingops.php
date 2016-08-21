2bc314ba06c13d28bb4b5112425ccd013f5cb12f
tom2tom <tpgww@onepost.net>
2016-08-21 20:15:26 +1000

    sql updates message updates

--- lib/class.Bookingops.php
+++ lib/class.Bookingops.php
@@ -20,83 +20,70 @@ class Bookingops
 	*/
 	private function GetBkgData(&$mod, $bkg_id)
 	{
+		$sql = <<<EOS
+SELECT D.*,B.name,B.publicid,B.address,B.phone,B.displayclass FROM {$mod->DataTable} D
+JOIN {$mod->BookerTable} B ON D.booker_id=B.booker_id
+WHERE D.bkg_id
+EOS;
 		if (is_array($bkg_id)) {
-			$fillers = str_repeat('?,',count($bkg_id)-1);
-			return $mod->dbHandle->GetAssoc('SELECT * FROM '.$mod->DataTable.' WHERE bkg_id IN ('.$fillers.'?)',$bkg_id);
+			return $mod->dbHandle->GetAssoc($sql.' IN ('.str_repeat('?,',count($bkg_id)-1).'?)',$bkg_id);
 		} else
-			return $mod->dbHandle->GetAssoc('SELECT * FROM '.$mod->DataTable.' WHERE bkg_id=?',array($bkg_id));
+			return $mod->dbHandle->GetAssoc($sql.'=?',array($bkg_id));
 	}
 
 	/*
 	MsgParms:
-	see also - bkrrequestops::MsgParms
-	Construct arguments for MessageSender::Send()
+	Construct arguments for 'outward' message using MessageSender::Send()
+	see also - Booker\Requestops::MsgParms
 	@mod: reference to current Booker module object
 	@utils: reference to Utils-class object
 	@bdata: reference to array of booking data
 	@idata: reference to array of booked-item data
-	@mtype: MSG* enum
+	@mtype: enum MSGCHANGED or MSGCANCELLED
 	@custommsg: text entered by user, to replace square-bracketed content of the message 'template'
 	@extra: optional stuff for some types of message, default ''
+	Returns: array or FALSE
 	*/
 	private function MsgParms(&$mod, &$utils, &$bdata, &$idata, $mtype, $custommsg, $extra='')
 	{
-		$overday = ($utils->GetInterval($mod,$idata['item_id'],'slot') >= 84600);
 		switch ($mtype) {
 		 case self::MSGCHANGED:
 			$ktitle = 'email_changed_title';
-			if ($overday) {
-				$kbody1 = 'email_changed';
-				$kbody2 = 'text_change';
-			} else {
-				$kbody1 = 'email_changedat';
-				$kbody2 = 'text_changeat';
-			}
+			$kbody1 = 'email_changed';
+			$kbody2 = 'text_change';
 		 	break;
 		 case self::MSGCANCELLED:
 			$ktitle = 'email_cancelled_title';
-			if ($overday) {
-				$kbody1 = 'email_cancel';
-				$kbody2 = 'text_cancel';
-			} else {
-				$kbody1 = 'email_cancelat';
-				$kbody2 = 'text_cancelat';
-			}
+			$kbody1 = 'email_cancel';
+			$kbody2 = 'text_cancel';
 			break;
 		 default:
 		 	return FALSE; //error
 		}
 
 		$from = FALSE; //always use default sender
-		$to = array($bdata['sender']=>$bdata['contact']);
+		$to = ($bdata['address']) ? array($bdata['name']=>$bdata['address']):$bdata['phone'];
+
 		$what = ($bdata['subgrpcount'] > 1) ?
 			sprintf('%d %s',$bdata['subgrpcount'],$idata['membersname']):
 			$utils->GetItemName($mod,$idata);
-		$dts = new \DateTime('1900-1-1',new \DateTimeZone('UTC'));
-		$dts->setTimestamp($bdata['slotstart']);
+		$dts = new \DateTime('@'.$bdata['slotstart'],new \DateTimeZone('UTC'));
 		$on = $utils->IntervalFormat($mod,$dts,'D j M');
-
-		$textparms = array('prefix'=>$idata['smsprefix'],'pattern'=>$idata['smspattern']);
-		$mailparms = array('subject'=>$mod->Lang($ktitle));
-		$tweetparms = array();
-		if ($overday) {
-			$msg = $mod->Lang($kbody1,$what,$on);
-			$msg = preg_replace('/\[.*\]/',$custommsg,$msg);
-			$mailparms['body'] = $msg;
-			$msg = $mod->Lang($kbody2,$what,$on);
-			$msg = preg_replace('/\[.*\]/',$custommsg,$msg);
-			$textparms['body'] = $msg;
-			$tweetparms['body'] = $msg;
+		if ($utils->GetInterval($mod,$idata['item_id'],'slot') >= 84600) {
+			$detail = $mod->Lang('whatovrday',$what,$on);
 		} else {
 			$at = $dts->format('g:i A');
-			$msg = $mod->Lang($kbody1,$what,$on,$at);
-			$msg = preg_replace('/\[.*\]/',$custommsg,$msg);
-			$mailparms['body'] = $msg;
-			$msg = $mod->Lang($kbody2,$what,$on,$at);
-			$msg = preg_replace('/\[.*\]/',$custommsg,$msg);
-			$textparms['body'] = $msg;
-			$tweetparms['body'] = $msg;
+			$detail = $mod->Lang('whatonday',$what,$on,$at);
 		}
+
+		$msg = $mod->Lang($kbody1,$detail);
+		$msg = preg_replace('/\[.*\]/',$custommsg,$msg);
+		$mailparms = array('subject'=>$mod->Lang($ktitle),'body'=>$msg);
+		$msg = $mod->Lang($kbody2,$detail);
+		$msg = preg_replace('/\[.*\]/',$custommsg,$msg);
+		$textparms = array('prefix'=>$idata['smsprefix'],
+			'pattern'=>$idata['smspattern'],'body'=>$msg);
+		$tweetparms = array('body'=>$msg);
 		return array($from,$to,$textparms,$mailparms,$tweetparms);
 	}
 
@@ -137,20 +124,16 @@ class Bookingops
 	ExportBkg:
 	Export data for one or more bookings
 	@mod: reference to current Booker module
-	@bkg_id: booking identifier, or array of them
+	@bkg_id: booking identifier, or array of them, or '*'
 	Returns: 2-member array, 1st is T/F indicating success, 2nd '' or error message
 	*/
 	public function ExportBkg(&$mod, $bkg_id)
 	{
-		$rows = self::GetBkgData($mod,$bkg_id);
-		if ($rows) {
-			$funcs = new CSV();
-			list($res,$key) = $funcs->ExportBookings($mod,FALSE,array_keys($rows));
-			if ($res)
-				return array(TRUE,'');
-			return array(FALSE,$mod->Lang($key));
-		} else
-			return array(FALSE,$mod->Lang('err_data'));
+		$funcs = new CSV();
+		list($res,$key) = $funcs->ExportBookings($mod,FALSE,$bkg_id);
+		if ($res)
+			return array(TRUE,'');
+		return array(FALSE,$mod->Lang($key));
 	}
 
 	/**
@@ -206,12 +189,17 @@ class Bookingops
 	public function DeleteRepeat(&$mod, $bkg_id)
 	{
 		$utils = new Utils();
+		$sql = <<<EOS
+SELECT R.*,B.name,B.address,B.phone FROM {$mod->RepeatTable} R
+JOIN {$mod->BookerTable} B ON R.booker_id=B.booker_id
+JOIN {$mod->DataTable} D ON R.booker_id=D.booker_id
+WHERE D.bkg_id
+EOS;
 		if (is_array($bkg_id)) {
-			$fillers = str_repeat('?,',count($bkg_id)-1);
-			$sql = 'SELECT * FROM '.$mod->RepeatTable.' WHERE bkg_id IN ('.$fillers.'?)';
+			$sql .= ' IN ('.str_repeat('?,',count($bkg_id)-1).'?)';
 			$rows = $utils->SafeGet($sql,$bkg_id,'assoc');
 		} else {
-			$sql = 'SELECT * FROM '.$mod->RepeatTable.' WHERE bkg_id=?';
+			$sql .= '=?';
 			$rows = $utils->SafeGet($sql,array($bkg_id),'assoc');
 		}
 
@@ -248,10 +236,11 @@ class Bookingops
 
 	private function CurrentBookingUser(&$mod, &$params)
 	{
-		if ($params['repeat'])
-			$sql = 'SELECT user FROM '.$mod->RepeatTable.' WHERE bkg_id=?';
-		else
-			$sql = 'SELECT user FROM '.$mod->DataTable.' WHERE bkg_id=?';
+		$sql = <<<EOS
+SELECT B.name FROM {$mod->BookerTable} B
+JOIN {$mod->DataTable} D ON B.booker_id=D.booker_id
+WHERE D.bkg_id=?
+EOS;
 		return $mod->dbHandle->GetOne($sql,array($params['bkg_id']));
 	}
 
@@ -267,38 +256,45 @@ class Bookingops
 		$utils = new Utils();
 		$old = FALSE;
 		$ret = TRUE;
-		if (!empty($params['conformcontact'])) {
+		if (!empty($params['conformuser'])) {
 			$old = self::CurrentBookingUser($mod,$params);
-			if (!$old) return;
-			$allsql = array(
-			'UPDATE '.$mod->DataTable.' SET contact=? WHERE user=?',
-			'UPDATE '.$mod->RepeatTable.' SET contact=? WHERE user=?');
-			$args = array($params['contact'],$old);
-			if (!$utils->SafeExec($allsql,array($args,$args)))
+			if (!$old) return FALSE;
+			$sql2 = '';
+			$args = array();
+			foreach (array('publicid','name') as $k) {
+				if (!empty($params[$k])) {
+					$sql2 .= $k.'=?';
+					$args[] = trim($params[$k]);
+				}
+			}
+			$args[] = $old;
+			$sql = 'UPDATE '.$mod->BookerTable.' SET '.$sql2.' WHERE booker_id=?';
+			if (!$utils->SafeExec($sql,$args)) {
+				$ret = FALSE;
+			} elseif(!(empty($params['publicid']) || empty($params['passwd']))) {
+					$ufuncs->SetPassword ($old,'FORCE',$params['passwd']); //TODO ufuncs
+			}
+		}
+
+		if (!empty($params['conformcontact'])) {
+			if (!$old) {
+				$old = self::CurrentBookingUser($mod,$params);
+				if (!$old) return FALSE;
+			}
+			$sql = 'UPDATE '.$mod->BookerTable.' SET address=?,phone=? WHERE booker_id=?';
+			$args = array(trim($params['address']),trim($params['phone']),$old);
+			if (!$utils->SafeExec($sql,$args))
 				$ret = FALSE;
 		}
 
 		if (!empty($params['conformstyle'])) {
-			if (!$old)
+			if (!$old) {
 				$old = self::CurrentBookingUser($mod,$params);
-			if (!$old) return;
-			$allsql = array(
-			'UPDATE '.$mod->DataTable.' SET displayclass=? WHERE user=?',
-			'UPDATE '.$mod->RepeatTable.' SET displayclass=? WHERE user=?');
+				if (!$old) return FALSE;
+			}
+			$sql = 'UPDATE '.$mod->BookerTable.' SET displayclass=? WHERE booker_id=?';
 			$args = array((int)$params['displayclass'],$old);
-			if (!$utils->SafeExec($allsql,array($args,$args)))
-				$ret = FALSE;
-		}
-
-		if (!empty($params['conformuser'])) {
-			if (!$old)
-				$old = self::CurrentBookingUser($mod,$params);
-			if (!$old) return;
-			$allsql = array(
-			'UPDATE '.$mod->DataTable.' SET user=? WHERE user=?',
-			'UPDATE '.$mod->RepeatTable.' SET user=? WHERE user=?');
-			$args = array($params['user'],$old);
-			if (!$utils->SafeExec($allsql,array($args,$args)))
+			if (!$utils->SafeExec($sql,$args))
 				$ret = FALSE;
 		}
 		return $ret;
@@ -306,58 +302,90 @@ class Bookingops
 
 	/**
 	SaveBkg:
+	Add to or update DataTable per data in @params
 	@mod: reference to current Booker module
 	@params: reference to parameters array
-	@is_new:
-	Returns: T/F indicating successful completion
+	@is_new: whether this is a new booking (hence add to table)
+	Returns: boolean indicating successful completion
 	*/
 	public function SaveBkg(&$mod, &$params, $is_new)
 	{
+		if (empty($params['booker_id'])) {
+	 		$funcs = new Userops($mod);
+			list($booker_id,$newbooker) = $funcs->GetParamsID($params);
+			if ($booker_id === FALSE) {
+				//TODO nicely handle bad password e.g. message
+				sleep(2);
+				return FALSE;
+			}
+		} else {
+			$booker_id = (int)$params['booker_id']; //TODO upstream admin must supply this
+		}
+
 		if (isset($params['when'])) {
-			$dts = new \DateTime($params['when'],new \DateTimeZone('UTC'));
-			$params['when'] = $dts->getTimestamp();
+			$dt = new \DateTime($params['when'],new \DateTimeZone('UTC'));
+			$t1 = $dt->getTimestamp();
 			if (isset($params['until'])) {
-				$dts->modify($params['until']);
-				$t = $dts->getTimestamp() - $params['when'];
-				if ($t < 60) $t = 60;
-				$params['until'] = $t;
+				$dt->modify($params['until']);
+				$t2 = $dt->getTimestamp() - $t1;
+				if ($t2 < 120) $t2 = 120; //2-minutes min.
+				$t2--; //TODO iff ends at any slot-start
 			}
 		}
+
 		if ($is_new) {
-			$sql2 = 'bkg_id,item_id,slotstart,user,contact,displayclass';
-			$fillers = '?,?,?,?,?,?';
+			$sql2 = 'bkg_id,item_id,booker_id';
+			$fillers = '?,?,?';
 			$bid = $mod->dbHandle->GenID($mod->DataTable.'_seq');
 			$args = array(
 				$bid,
 				(int)$params['item_id'],
-				$params['user'],
-				$params['contact'],
-				(int)$params['displayclass']
+				$booker_id
 			);
 			foreach (array('when','until','paid') as $k) {
 				if (isset($params[$k])) {
+					switch ($k) {
+					 case 'when':
+						$args[] = $t1;
+						$k = 'slotstart';
+						break;
+					 case 'until':
+						$args[] = $t2;
+						$k = 'slotlen';
+						break;
+					 case 'paid':
+						$args[] = (int)$params[$k];
+						break;
+					}
 					$sql2 .= ",$k";
 					$fillers .= ',?';
-					$args[] = (int)$params[$k];
 				}
 			}
 			$sql = 'INSERT INTO '.$mod->DataTable.' ('.$sql2.') VALUES ('.$fillers.')';
 		} else { //update
 			self::ConformBookingData($mod,$params); //general update where needed
-			$sql2 = 'slotstart=?,user=?,contact=?,displayclass=?';
-			$args = array(
-				$params['user'],
-				$params['contact'],
-				(int)$params['displayclass']
-			);
+			$sql2 = array();
+			$args = array();
 			foreach (array('when','until','paid') as $k) {
 				if (isset($params[$k])) {
-					$sql2 .= ",$k=?";
-					$args[] = (int)$params[$k];
+					switch ($k) {
+					 case 'when':
+						$args[] = $params[$k];
+						$k = 'slotstart';
+						break;
+					 case 'until':
+						$args[] = $params[$k];
+						$k = 'slotlen';
+						break;
+					 case 'paid':
+						$args[] = (int)$params[$k];
+						break;
+					}
+					$sql2[] = "$k=?";
 				}
 			}
 			$args[] = (int)$params['bkg_id'];
-			$sql = 'UPDATE '.$mod->DataTable.' SET '.$sql2.' WHERE bkg_id=?';
+			$sql = 'UPDATE '.$mod->DataTable.' SET '.implode(',',$sql2).' WHERE bkg_id=?';
 		}
 		$utils = new Utils();
 		return $utils->SafeExec($sql,$args);
@@ -376,9 +404,10 @@ class Bookingops
 	public function GetBooked(&$mod, $item_id, $startstamp, $endstamp)
 	{
 		$sql = <<<EOS
-SELECT B.*,I.name FROM {$mod->DataTable} B
-LEFT JOIN {$mod->ItemTable} I ON B.item_id = I.item_id
-WHERE B.item_id
+SELECT D.*,B.name AS user,I.name FROM {$mod->DataTable} D
+LEFT JOIN {$mod->BookerTable} B ON D.booker_id = B.booker_id
+LEFT JOIN {$mod->ItemTable} I ON D.item_id = I.item_id
+WHERE D.item_id
 EOS;
 		if (is_array($item_id)) {
 			$args = $item_id;
@@ -398,7 +427,7 @@ EOS;
 		}
 		$args[] = $endstamp;
 		$args[] = $startstamp;
-		$sql .= ' AND B.slotstart <= ? AND (B.slotstart+B.slotlen) >= ? ORDER BY I.name,B.slotstart';
+		$sql .= ' AND D.slotstart <= ? AND (D.slotstart+D.slotlen) >= ? ORDER BY I.name,D.slotstart';
 		$utils = new Utils();
 		return $utils->SafeGet($sql,$args);
 	}
@@ -421,10 +450,10 @@ EOS;
 			$args = $item_id;
 		$fillers = str_repeat('?,',count($args)-1);
 		$sql = <<<EOS
-SELECT B.*,I.name FROM {$mod->DataTable} B
-JOIN {$mod->ItemTable} I ON B.item_id = I.item_id
-WHERE B.item_id IN ({$fillers}?) AND B.slotstart <= ? AND (B.slotstart+B.slotlen) >= ?
-ORDER BY B.slotstart,I.name
+SELECT D.*,I.name FROM {$mod->DataTable} D
+JOIN {$mod->ItemTable} I ON D.item_id = I.item_id
+WHERE D.item_id IN ({$fillers}?) AND D.slotstart <= ? AND (D.slotstart+D.slotlen) >= ?
+ORDER BY D.slotstart,I.name
 EOS;
 		$args[] = $endstamp;
 		$args[] = $startstamp;
@@ -452,26 +481,27 @@ EOS;
 			$args = $item_id;
 		$fillers = str_repeat('?,',count($args)-1);
 		$sql = <<<EOS
-SELECT B.slotstart,B.slotlen,B.user,I.name FROM {$mod->DataTable} B
-LEFT JOIN {$mod->ItemTable} I ON B.item_id = I.item_id
-WHERE B.item_id IN ({$fillers}?) AND B.slotstart <= ? AND (B.slotstart+B.slotlen) >= ?
+SELECT D.slotstart,D.slotlen,B.name AS user,I.name FROM {$mod->DataTable} D
+JOIN {$mod->BookerTable} B ON D.booker_id = B.booker_id
+JOIN {$mod->ItemTable} I ON D.item_id = I.item_id
+WHERE D.item_id IN ({$fillers}?) AND D.slotstart <= ? AND (D.slotstart+D.slotlen) >= ?
 ORDER BY
 EOS;
 		switch ($lfmt) {
 		 case \Booker::LISTUS:
 			$t = ($is_group) ? 'I.name,':'';
-			$sql .= ' B.user,'.$t.'B.slotstart';
+			$sql .= ' B.name,'.$t.'D.slotstart';
 			break;
 		 case \Booker::LISTRS:
-			$sql .= ' B.user,B.slotstart';
+			$sql .= ' B.name,D.slotstart';
 			if ($is_group) $sql .= ',I.name';
 			break;
 		 case \Booker::LISTSR: //only for groups
-			$sql .= ' I.name,B.slotstart,B.user';
+			$sql .= ' I.name,D.slotstart,B.name';
 			break;
 //	 case \Booker::LISTSU:
 		 default:
-			$sql .= ' B.slotstart,B.user';
+			$sql .= ' D.slotstart,B.name';
 			if ($is_group) $sql .= ',I.name';
 			break;
 		}
