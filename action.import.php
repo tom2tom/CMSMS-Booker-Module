8b7a70d28f9ceac66dddb1e27030e62fdb945e84
tom2tom <tpgww@onepost.net>
2016-08-24 17:51:09 +1000

    adapt for extra import-types

--- action.import.php
+++ action.import.php
@@ -28,22 +28,47 @@ if (isset($params['cancel'])) {
 	$this->Redirect($id,$params['resume'],'',$parms);
 }
 
-$imitm = !empty($params['importitm']);
-$imbkg = !empty($params['importbkg']);
-if (!($imitm || $imbkg)) exit;
+if (!empty($params['importitm']))
+	$itype = 1;
+elseif (!empty($params['importbkr']))
+	$itype = 2;
+elseif (!empty($params['importbkg']))
+	$itype = 3;
+elseif (!empty($params['importfee']))
+	$itype = 4;
+elseif (!empty($params['importhist']))
+	$itype = 5;
+else
+	exit;
 
 if (isset($_FILES) && isset($_FILES[$id.'csvfile'])) {
-	$funcs = new Booker\CSV();
-	if ($imitm) {
+	$funcs = new Booker\Import();
+	switch ($itype) {
+	 case 1:
 		list($res,$prop) = $funcs->ImportItems($this,$id);
-	} else { //$imbkg
+		$k = 'itemv_multi';
+		break;
+	 case 2:
+		list($res,$prop) = $funcs->ImportBookers($this,$id);
+		$k = 'booker_multi';
+		break;
+	 case 3:
 		$single = (isset($params['item_id'])) ? (int)$params['item_id']:FALSE;
 		list($res,$prop) = $funcs->ImportBookings($this,$id,$single);
+		$k = 'booking_multi';
+		break;
+	 case 4:
+		list($res,$prop) = $funcs->ImportFees($this,$id);
+		$k = 'fee_multi';
+		break;
+	 case 5:
+		list($res,$prop) = $funcs->ImportHistory($this,$id);
+		$k = 'history_multi'; //TODO or request_multi?
+		break;
 	}
 
 	if ($res) {
-		$t = ($imitm) ? $this->Lang('itemv_multi'):$this->Lang('booking_multi');
-		$t = $this->Lang('import_result',$prop,$t);
+		$t = $this->Lang('import_result',$prop,$this->Lang($k));
 		$msg = $this->_PrettyMessage($t,TRUE,FALSE);
 	} else
 		$msg = $this->_PrettyMessage($prop,FALSE);
@@ -58,6 +83,7 @@ $hidden = array();
 switch ($params['action']) {
  case 'process': //items
  case 'adminbooking': //bookings
+ case 'adminbooker': //bookers
 	$hidden['resume'] = 'defaultadmin';
 	$hidden['active_tab'] = $params['active_tab'];
 	break;
@@ -69,20 +95,43 @@ switch ($params['action']) {
  default:
 	$this->Crash();
 }
-if ($imbkg)
-	$hidden['importbkg'] = $params['importbkg'];
-else //$imitm
-	$hidden['importitm'] = $params['importitm'];
+
+switch ($itype) {
+ case 1:
+	$k = 'importitm';
+	$k2 = 'title_importitems';
+	$k3 = 'help_importitem';
+	break;
+ case 2:
+	$k = 'importbkr';
+	$k2 = 'title_importbookers';
+	$k3 = 'help_importbooker';
+	break;
+ case 3:
+	$k = 'importbkg';
+ 	$k2 = 'title_importbooks';
+ 	$k3 = 'help_importbooking';
+	break;
+ case 4:
+	$k = 'importfee';
+ 	$k2 = 'title_importfees';
+ 	$k3 = 'help_importfee';
+	break;
+ case 5:
+	$k = 'importhist';
+ 	$k2 = 'title_importhists';
+ 	$k3 = 'help_importhistory';
+	break;
+}
+$hidden[$k] = 1;
 
 $tplvars['startform'] = $this->CreateFormStart($id,'import',$returnid,'POST',
 	'multipart/form-data','','',$hidden);
 $tplvars['endform'] = $this->CreateFormEnd();
-$k = ($imbkg) ? 'title_importbooks':'title_importitems';
-$tplvars['title'] = $this->Lang($k);
+$tplvars['title'] = $this->Lang($k2);
 $tplvars['chooser'] = $this->CreateInputFile($id,'csvfile','text/csv',25);
 $tplvars['apply'] = $this->CreateInputSubmit($id,'import',$this->Lang('upload'));
 $tplvars['cancel'] = $this->CreateInputSubmit($id,'cancel',$this->Lang('cancel'));
-$k = ($imbkg) ? 'help_importbooking':'help_importitem';
-$tplvars['help'] = $this->Lang($k);
+$tplvars['help'] = $this->Lang($k3);
 
 echo Booker\Utils::ProcessTemplate($this,'chooser.tpl',$tplvars);
