c5863a1757b94fa878c615a756424d9e8eba6775
tom2tom <tpgww@onepost.net>
2016-09-21 09:13:39 +1000

    new action

---
+++ action.announce.php
@@ -0,0 +1,34 @@
+<?php
+#----------------------------------------------------------------------
+# Module: Booker - a resource booking module
+# Action: announce - display message
+#----------------------------------------------------------------------
+# See file Booker.module.php for full details of copyright, licence, etc.
+#----------------------------------------------------------------------
+
+//parameter keys filtered out before redirect etc
+$localparams = array(
+	'action',
+	'cancel',
+	'message'
+);
+
+$utils = new Booker\Utils();
+$utils->UnFilterParameters($params);
+
+if(isset($params['cancel']) || empty($params['message'])) {
+	$resume = array_pop($params['resume']);
+	$newparms = $utils->FilterParameters($params,$localparams);
+	$this->Redirect($id,$resume,$params['returnid'],$newparms);
+}
+
+$hidden = $utils->FilterParameters($params,$localparams);
+$tplvars = array(
+//	'title' => $this->Lang('title_requeststatus'),
+	'message'=> $params['message'],
+	'startform' => $this->CreateFormStart($id,'findbooking',$returnid,'POST','','','',$hidden),
+	'endform' => $this->CreateFormEnd(),
+	'cancel' => $this->CreateInputSubmit($id,'cancel',$this->Lang('close'))
+);
+
+echo Booker\Utils::ProcessTemplate($this,'announce.tpl',$tplvars);
