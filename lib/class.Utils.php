e1b21f1e8e8a18974362620b374f645e52b664e7
tom2tom <tpgww@onepost.net>
2016-09-26 21:29:30 +1000

    add status methods

--- lib/class.Utils.php
+++ lib/class.Utils.php
@@ -1744,6 +1744,52 @@ $this->Crash();
 		return $t;
 	}
 
+	/*
+	$wanted = single STAT* enum or array of those or '*'
+	*/
+	private function StatusKeys($wanted)
+	{
+		$stats = array(
+		 \Booker::STATNONE => 'stat_none',//unknown/normal/default
+		 \Booker::STATNEW => 'stat_new',//new, approver consideration pending
+		 \Booker::STATTELL => 'stat_tell',//further information submitted
+		 \Booker::STATCHG => 'stat_chg',//change request, approver consideration pending
+		 \Booker::STATDEFER => 'stat_defer',//request not yet processed cuz' too far ahead
+		 \Booker::STATDEL => 'stat_del',//delete request, approver consideration pending
+		 \Booker::STATTEMP => 'stat_temp',//user-recorded, pending admin confirmation
+		 \Booker::STATASK => 'stat_ask',//booker queried, waiting for response
+		 \Booker::STATCANCEL => 'stat_cancel',//abandoned by user or admin on user's behalf
+		 \Booker::STATADMINREC => 'stat_rec',//booking recorded by admin
+		 \Booker::STATBIG => 'stat_big',//too many slots requested
+		 \Booker::STATDEFERRED => 'stat_defer',//booking to be re-scheduled, per user request or admin imposition
+		 \Booker::STATDUP => 'stat_dup',//duplicate request, cannot accept
+		 \Booker::STATERR => 'stat_err',//system error while processing
+		 \Booker::STATFAILED => 'stat_fail',//generic request-failure
+		 \Booker::STATGONE => 'stat_gone',//deletion pending, while its historical data needed
+		 \Booker::STATLATE => 'stat_late',//request past or not far-enough ahead
+		 \Booker::STATNA => 'stat_na',//resouce N/A at requested time, cannot accept
+		 \Booker::STATOK => 'stat_approved',//aka APPROVED done/processed
+		 \Booker::STATRETRY => 'stat_retry',//some temporary problem, try again later
+		 \Booker::STATSELFREC => 'stat_selfrec'//recorded by approved user (i.e. no request)
+		);
+	
+		if (is_array($wanted)) {
+			$ret = array();
+			foreach($wanted as $t) {
+				if (array_key_exists($t,$stats)) {
+					$ret[$t] = $stats[$t];
+				}
+			}
+			return array_unique($ret);
+		} elseif ($wanted == '*') {
+			return $stats;
+		} elseif (array_key_exists($wanted,$stats)) {
+			return $stats[$wanted];
+		} else {
+			return 'err_parm';
+		}		
+	}
+
 	/**
 	GetStatus:
 	@params: variables to be used to determine the status
@@ -1751,55 +1797,48 @@ $this->Crash();
 	*/
 	public function GetStatus($params)
 	{
+		//stage,payable,paid,will-pay,overdue etc
 		return \Booker::STATOK; //TODO
+	}
+
+	public function GetStatusName(&$mod, $status)
+	{
+		$key = self::StatusKeys($status);
+		if (is_string($key))
+			return $mod->Lang($key);
+		return $key;
 	}
 
 	/**
 	GetStatusChoices:
 	@mod: reference to current module-object
-	@mode:
-	Returns: array suitable for dropdown picklist
+	@mode: bitflags:
+	 0 >> request-status,
+	 1 >> post-request ok etc,
+	 2 >> post-request problem
+	 3 >> payment status
+	Returns: array suitable for dropdown picklist, keys = text or number, values = STAT* enum
 	*/
 	public function GetStatusChoices(&$mod, $mode)
 	{
-/*	const STATNONE = 0;//unknown/normal/default
-	//request-stage
-	const STATNEW = 1;//new, approver consideration pending
-	const STATCHG = 2;//change request, approver consideration pending
-	const STATDEL = 3;//delete request, approver consideration pending
-	const STATTELL = 4;//further information submitted
-	const STATASK = 5;//booker queried, waiting for response
- 	const STATCANCEL = 6;//abandoned by user or admin on user's behalf
-	const STATMAXREQ = 9;//last-recognised request-status value
-	//later status
-	const STATOK = 20;//aka APPROVED done/processed
-	const STATADMINREC = 21;//booking recorded by admin
-	const STATSELFREC = 22;//recorded by approved user (i.e. no request)
-	const STATTEMP = 23;//user-recorded, pending admin confirmation
-	const STATDEFERRED = 40;//booking to be re-scheduled, per user request or admin imposition
- 	const STATGONE = 90;//deletion pending, while its historical data needed
-	//problems
-	const STATBIG = 80;//too many slots requested
-	const STATDEFER = 81;//request not yet processed cuz' too far ahead
-	const STATLATE = 82;//request past or not far-enough ahead
-	const STATNA = 83;//resouce N/A at requested time, cannot accept
-	const STATDUP = 84;//duplicate request, cannot accept
- 	const STATERR = 85;//system error while processing
- 	const STATRETRY = 86;//some temporary problem, try again later
-	const STATFAILED = 89;//generic request-failure
-	//HistoryTable payment codes
-	const STATFREE = 0;//no fee for use
-	const STATPAYABLE = 1;//fee applies, not yet paid
-	const STATPAID = 2;//fee pre- or post-paid
-	const STATCREDITED = 3;//fee to be paid upon request
- 	const STATNOTPAID = 9;//payable but unpaid for some non-credit-related reason
-	const STATCREDITUSED = 10;//past credit offset against other use
-	const STATCREDITEXPIRED = 11;//past credit timed out
-	const STATCREDITADDED = 12;//prepayment amount
-*/
-		$choices = array(); //TODO
+		if ($mode & 1) {
+			$wanted = range(\Booker::STATNONE,\Booker::STATMAXREQ);
+		} else {
+			$wanted = array();
+		}
+		if ($mode & 2) {
+			$wanted = array_merge($wanted,range(\Booker::STATMAXREQ+1,\Booker::STATMAXOK));
+		}
+		if ($mode & 4) {
+			$wanted = array_merge($wanted,range(\Booker::STATMAXOK+1,\Booker::STATMAXBAD));
+		}
+		if ($mode & 8) {
+			$wanted = array_merge($wanted,range(\Booker::STATFREE,\Booker::STATMAXPAY));
+		}
+		$choices = self::StatusKeys($wanted);
 		foreach ($choices as &$key) {
-			$key = $mod->Lang($key);
+			if (is_string($key))
+				$key = $mod->Lang($key);
 		}
 		unset($key);
 		return array_flip($choices);
