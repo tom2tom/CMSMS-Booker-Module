d02a06f18309d7b12f287c959b5e2472be8ebef1
tom2tom <tpgww@onepost.net>
2016-09-27 09:27:06 +1000

    export & import methods

--- lib/class.Utils.php
+++ lib/class.Utils.php
@@ -1,7 +1,7 @@
 <?php
 #----------------------------------------------------------------------
 # Module: Booker - a resource booking module
-# Library file: Shared
+# Library file: Utils
 #----------------------------------------------------------------------
 # See file Booker.module.php for full details of copyright, licence, etc.
 #----------------------------------------------------------------------
@@ -875,6 +875,67 @@ class Utils
 	}
 
 	/**
+	BuildResume:
+	Create (as array) or update @params['resume']
+	@resume: name of action
+	$params: reference to array of request-parameters
+	Returns: nothing
+	*/
+	public function BuildResume($resume, &$params)
+	{
+		if (empty($params['resume'])) {
+			$params['resume'] = array($resume);
+		} elseif (!is_array($params['resume'])) {
+			if ($params['resume'] != $resume) {
+				$params['resume'] = array($params['resume'],$resume);
+			} else {
+				$params['resume'] = array($resume);
+			}
+		} elseif (reset($params['resume']) != $resume) {
+				$params['resume'][] = $resume; 	
+		}
+	}
+
+	/**
+	BuildNav:
+	Generate XHTML page-change links for admin action
+	@mod: reference to current module-object
+	@id: session identifier
+	@returnid:
+	@resume: action name
+	@params: reference to array of request-parameters including link-related data
+	Returns: string, maybe ''
+	*/
+	public function BuildNav(&$mod, $id, $returnid, $resume, &$params)
+	{
+		$this->BuildResume($resume,$params);
+		$navstr = '';
+		foreach ($params['resume'] as $action) {
+			switch($action) {
+			 case 'itembookings':
+				$key = 'title_bookings';
+				$xtra = array('item_id'=>$params['item_id'],'task'=>$params['task']);
+				break;
+			 case 'bookerbookings':
+				$key = 'title_bookings';
+				$xtra = array('booker_id'=>$params['booker_id'],'task'=>$params['task']);
+				break;
+			 default:
+				$action = 'defaultadmin';
+				$key = 'back_module';
+				if (empty($params['active_tab'])) {
+					$xtra = array();
+				} else {
+					$xtra = array('active_tab'=>$params['active_tab']);
+				}
+				break;
+			}
+			$navstr .= $mod->CreateLink($id,$action,$returnid,'&#171; '.$mod->Lang($key),$xtra);
+		}
+		return $navstr;
+	}
+
+	/**
 	GetUploadsPath:
 	Get file-system-directory used for this module's uploads. No permission-checks.
 	@mod: reference to current module-object
@@ -1744,106 +1805,6 @@ $this->Crash();
 		return $t;
 	}
 
-	/*
-	$wanted = single STAT* enum or array of those or '*'
-	*/
-	private function StatusKeys($wanted)
-	{
-		$stats = array(
-		 \Booker::STATNONE => 'stat_none',//unknown/normal/default
-		 \Booker::STATNEW => 'stat_new',//new, approver consideration pending
-		 \Booker::STATTELL => 'stat_tell',//further information submitted
-		 \Booker::STATCHG => 'stat_chg',//change request, approver consideration pending
-		 \Booker::STATDEFER => 'stat_defer',//request not yet processed cuz' too far ahead
-		 \Booker::STATDEL => 'stat_del',//delete request, approver consideration pending
-		 \Booker::STATTEMP => 'stat_temp',//user-recorded, pending admin confirmation
-		 \Booker::STATASK => 'stat_ask',//booker queried, waiting for response
-		 \Booker::STATCANCEL => 'stat_cancel',//abandoned by user or admin on user's behalf
-		 \Booker::STATADMINREC => 'stat_rec',//booking recorded by admin
-		 \Booker::STATBIG => 'stat_big',//too many slots requested
-		 \Booker::STATDEFERRED => 'stat_defer',//booking to be re-scheduled, per user request or admin imposition
-		 \Booker::STATDUP => 'stat_dup',//duplicate request, cannot accept
-		 \Booker::STATERR => 'stat_err',//system error while processing
-		 \Booker::STATFAILED => 'stat_fail',//generic request-failure
-		 \Booker::STATGONE => 'stat_gone',//deletion pending, while its historical data needed
-		 \Booker::STATLATE => 'stat_late',//request past or not far-enough ahead
-		 \Booker::STATNA => 'stat_na',//resouce N/A at requested time, cannot accept
-		 \Booker::STATOK => 'stat_approved',//aka APPROVED done/processed
-		 \Booker::STATRETRY => 'stat_retry',//some temporary problem, try again later
-		 \Booker::STATSELFREC => 'stat_selfrec'//recorded by approved user (i.e. no request)
-		);
-	
-		if (is_array($wanted)) {
-			$ret = array();
-			foreach($wanted as $t) {
-				if (array_key_exists($t,$stats)) {
-					$ret[$t] = $stats[$t];
-				}
-			}
-			return array_unique($ret);
-		} elseif ($wanted == '*') {
-			return $stats;
-		} elseif (array_key_exists($wanted,$stats)) {
-			return $stats[$wanted];
-		} else {
-			return 'err_parm';
-		}		
-	}
-
-	/**
-	GetStatus:
-	@params: variables to be used to determine the status
-	Returns: a suitable \Booker::STAT* constant
-	*/
-	public function GetStatus($params)
-	{
-		//stage,payable,paid,will-pay,overdue etc
-		return \Booker::STATOK; //TODO
-	}
-
-	public function GetStatusName(&$mod, $status)
-	{
-		$key = self::StatusKeys($status);
-		if (is_string($key))
-			return $mod->Lang($key);
-		return $key;
-	}
-
-	/**
-	GetStatusChoices:
-	@mod: reference to current module-object
-	@mode: bitflags:
-	 0 >> request-status,
-	 1 >> post-request ok etc,
-	 2 >> post-request problem
-	 3 >> payment status
-	Returns: array suitable for dropdown picklist, keys = text or number, values = STAT* enum
-	*/
-	public function GetStatusChoices(&$mod, $mode)
-	{
-		if ($mode & 1) {
-			$wanted = range(\Booker::STATNONE,\Booker::STATMAXREQ);
-		} else {
-			$wanted = array();
-		}
-		if ($mode & 2) {
-			$wanted = array_merge($wanted,range(\Booker::STATMAXREQ+1,\Booker::STATMAXOK));
-		}
-		if ($mode & 4) {
-			$wanted = array_merge($wanted,range(\Booker::STATMAXOK+1,\Booker::STATMAXBAD));
-		}
-		if ($mode & 8) {
-			$wanted = array_merge($wanted,range(\Booker::STATFREE,\Booker::STATMAXPAY));
-		}
-		$choices = self::StatusKeys($wanted);
-		foreach ($choices as &$key) {
-			if (is_string($key))
-				$key = $mod->Lang($key);
-		}
-		unset($key);
-		return array_flip($choices);
-	}
-
 	/**
 	StripTags:
 	Remove and/or modify substring(s) of @str which are surrounded by <> and in @tags[]
@@ -1873,6 +1834,13 @@ $this->Crash();
 				'$1'.chr(255).'$2',htmlentities($str,ENT_QUOTES,'UTF-8'));
 			},$array),$array);
 		}
+	}
+
+	public function mb_ksort(&$array)
+	{
+		$swap = array_flip($array);
+		$this->mb_asort($swap);
+		$array = array_flip($swap);
 	}
 
 	/**
@@ -1963,36 +1931,34 @@ $this->Crash();
 				}
 			}
 			$patn = '/[\'"] ?[Oo][Rr] ?["\']/';
-			foreach ($include as $key) {
-				if (isset($params[$key])) {
-					$val = $params[$key];
-					if (!is_array($val)) {
-						if (is_string($val) && $val) {
-							if (is_numeric($val)) {
-								$i = (int)$val;
-								$params[$key] = ($i == $val) ? $i : (float)$val;
+			foreach ($include as $k) {
+				if (isset($params[$k])) {
+					$v = $params[$k];
+					if (!is_array($v)) {
+						if (is_string($v) && $v) {
+							if (is_numeric($v)) {
+								$params[$k] = $v + 0;
 							} else {
-								if (strpos($val,'&') !== FALSE) {
-									$val = html_entity_decode($val,ENT_QUOTES|ENT_HTML401);
+								if (strpos($v,'&') !== FALSE) {
+									$v = html_entity_decode($v,ENT_QUOTES|ENT_HTML401);
 								}
-								$val = str_replace('`','',$val);
-								$val = preg_replace($patn,'_',$val);
-								$params[$key] = $val;
+								$v = str_replace('`','',$v);
+								$v = preg_replace($patn,'_',$v);
+								$params[$k] = $v;
 							}
 						}
 					} else {
-						foreach ($val as $j=>&$one) {
+						foreach ($v as $i=>&$one) {
 							if (is_string($one) && $one) {
 								if (is_numeric($one)) {
-									$i = (int)$one;
-									$params[$key][$j] = ($i == $one) ? $i : (float)$one;
+									$params[$k][$i] = $one + 0;
 								} else {
 									if (strpos($one,'&') !== FALSE) {
 										$one = html_entity_decode($one,ENT_QUOTES|ENT_HTML401);
 									}
 									$one = str_replace('`','',$one);
 									$one = preg_replace($patn,'_',$one);
-									$params[$key][$j] = $one;
+									$params[$k][$i] = $one;
 								}
 							}
 						}
