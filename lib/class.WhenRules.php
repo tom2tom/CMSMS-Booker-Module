548e76d77df1e1fc77bd1850ac378ff26eb0ef09
tom2tom <tpgww@onepost.net>
2016-10-03 12:42:00 +1100

    change much API to use timestamps instead of DateTimes correct sun-related calcs

--- lib/class.WhenRules.php
+++ lib/class.WhenRules.php
@@ -16,7 +16,7 @@ class WhenRules extends WhenRuleLexer
 
 	/*
 	PeriodBlocks:
-	Append to @starts[] and @ends[] pair(s) of timestamps in $bs..$be and
+	Append to @starts[] and @ends[] pair(s) of timestamps in $bs..$be-1 and
 		consistent with @cond
 	@cond: member of parent::conds[] with parsed components of an interval-descriptor
 		=>['P'] will be populated, =>['T'] may be populated
@@ -38,12 +38,12 @@ class WhenRules extends WhenRuleLexer
 			foreach ($cond['T'] as $one) {
 				if (is_array($one)) {
 					foreach ($one as $t) {
-						if (strpos($t,'R') !== FALSE || strpos($t,'S') !== FALSE) {
+						if (strpos($t,'RS') !== FALSE || strpos($t,'SS') !== FALSE) {
 							$sunny = TRUE;
 							break;
 						}
 					}
-				} elseif (strpos($one,'R') !== FALSE || strpos($one,'S') !== FALSE) {
+				} elseif (strpos($one,'RS') !== FALSE || strpos($one,'SS') !== FALSE) {
 					$sunny = TRUE;
 					break;
 				}
@@ -252,11 +252,13 @@ class WhenRules extends WhenRuleLexer
 	GetTimeBlock:
 	Get timestamps for start & end of intra-day block represented by @timedata
 	@timedata: a member of a $cond['T'] i.e. a string or 3-member array
-	@bs: stamp for start of day being procesed
-	@be: stamp for 1-past-end of day being procesed
+	@bs: timestamp for start of day being procesed
+	@be: timestamp for 1-past-end of day being procesed
 	@dtw: modifiable DateTime object for use in relative calcs
 	@timeparms: reference to array of parameters from self::TimeParms
-	Returns: array(blockstart,blockend) or array(FALSE,FALSE)
+	Returns: 2-member array:
+		[0] = @bs-relative blockstart or FALSE
+		[1] = @bs-relative blockend or FALSE
 	*/
 	private function GetTimeBlock($timedata, $bs, $be, $dtw, &$timeparms)
 	{
@@ -279,7 +281,7 @@ class WhenRules extends WhenRuleLexer
 			$parts = array($timedata,$dtw->format('G:i'));
 		}
 		//block-start
-		if (strpos($parts[0],'R') !== FALSE) {
+		if (strpos($parts[0],'RS') !== FALSE) { //involves sunrise
 			/*
 			Sunrise $zenith=90+50/60
 			Twilights: see http://www.timeanddate.com/astronomy/about-sun-calculator.html
@@ -287,35 +289,43 @@ class WhenRules extends WhenRuleLexer
 			Nautical twilight $zenith=102.0
 			Astronomical twilight $zenith=108.0
 			*/
-			$tbase = date_sunrise($bs,SUNFUNCS_RET_TIMESTAMP,$timeparms['lat'],$timeparms['long'],96.0,$timeparms['gmtoff']);
-			$parts[0] = str_replace('R','',$parts[0]);
-		} elseif (strpos($parts[0],'S') !== FALSE) {
-			$tbase = date_sunset($bs,SUNFUNCS_RET_TIMESTAMP,$timeparms['lat'],$timeparms['long'],96.0,$timeparms['gmtoff']);
-			$parts[0] = str_replace('S','',$parts[0]);
-		} else {
+			$tbase = date_sunrise($bs,SUNFUNCS_RET_TIMESTAMP,$timeparms['lat'],$timeparms['long'],96.0,0) +
+				$timeparms['gmtoff'];
+			$parts[0] = str_replace('RS','',$parts[0]);
+		} elseif (strpos($parts[0],'SS') !== FALSE) { //involves sunset
+			$tbase = date_sunset($bs,SUNFUNCS_RET_TIMESTAMP,$timeparms['lat'],$timeparms['long'],96.0,0) +
+				$timeparms['gmtoff'];
+			$parts[0] = str_replace('SS','',$parts[0]);
+		} else { //not sunny
 			$tbase = $bs;
 		}
-		$dtw->setTimestamp($tbase-$bs);
+		$dtw->setTimestamp($tbase);
 		self::RelTime($dtw,$parts[0]);
 		$s = $dtw->getTimestamp();
-		if ($s < 0 || $s >= $be-$bs) {
+		if ($s < $bs || $s >= $be) {
 			$s = 0;
+		} else {
+			$s -= $bs;
 		}
 		//block-end
-		if (strpos($parts[1],'R') !== FALSE) {
-			$tbase = date_sunrise($bs,SUNFUNCS_RET_TIMESTAMP,$timeparms['lat'],$timeparms['long'],96.0,$timeparms['gmtoff']);
-			$parts[1] = str_replace('R','',$parts[1]);
-		} elseif (strpos($parts[1],'S') !== FALSE) {
-			$tbase = date_sunset($bs,SUNFUNCS_RET_TIMESTAMP,$timeparms['lat'],$timeparms['long'],96.0,$timeparms['gmtoff']);
-			$parts[1] = str_replace('S','',$parts[1]);
-		} else {
+		if (strpos($parts[1],'RS') !== FALSE) { //sunrise
+			$tbase = date_sunrise($bs,SUNFUNCS_RET_TIMESTAMP,$timeparms['lat'],$timeparms['long'],96.0,0) +
+				$timeparms['gmtoff'];
+			$parts[1] = str_replace('RS','',$parts[1]);
+		} elseif (strpos($parts[1],'SS') !== FALSE) { //sunset
+			$tbase = date_sunset($bs,SUNFUNCS_RET_TIMESTAMP,$timeparms['lat'],$timeparms['long'],96.0,0) +
+				$timeparms['gmtoff'];
+			$parts[1] = str_replace('SS','',$parts[1]);
+		} else { //no sun
 			$tbase = $bs;
 		}
-		$dtw->setTimestamp($tbase-$bs);
+		$dtw->setTimestamp($tbase);
 		self::RelTime($dtw,$parts[1]);
 		$e = $dtw->getTimestamp();
-		if ($e < 0 || $e >= $be-$bs) {
+		if ($e < $bs || $e >= $be) {
 			$e = $be-$bs-1;
+		} else {
+			$e -= $bs;
 		}
 		if ($e > $s)
 			return array($s,$e);
@@ -324,7 +334,7 @@ class WhenRules extends WhenRuleLexer
 
 	/*
 	TimeBlocks:
-	Get block-timestamps consistent with @cond and in $bs..$bs + 1 day - 1 second
+	Get block-timestamps for the entire day which includes @bs and consistent with @cond
 	@cond: reference to 'T'-member of one of parent::conds[]
 	@bs: stamp somwhere in the day being processed
 	@dtw: modifiable DateTime object for use in relative calcs
@@ -354,8 +364,8 @@ class WhenRules extends WhenRuleLexer
 				$at = $dtw->format('Y-m-d');
 				try {
 					$tz = new \DateTimeZone($timeparms['zone']);
-					$dt = new \DateTime($at,$tz);
-					$offs = $dt->format('Z')/3600; //DST-specific
+					$dt2 = new \DateTime($at,$tz);
+					$offs = $dt2->format('Z'); //DST-specific, unlike DateTimeZone::getOffset
 				} catch (Exception $e) {
 					$offs = 0;
 				}
@@ -380,7 +390,7 @@ class WhenRules extends WhenRuleLexer
 				continue;
 			}
 			list($gets,$gete) = self::GetTimeBlock($one,$bs,$be,$dtw,$timeparms);
-			if ($gets) {
+			if ($gete) { //$gets may be 0
 				$starts[] = $gets;
 				$ends[] = $gete;
 			}
@@ -399,7 +409,7 @@ class WhenRules extends WhenRuleLexer
 				continue;
 			}
 			list($gets,$gete) = self::GetTimeBlock($one,$bs,$be,$dtw,$timeparms);
-			if ($gets) {
+			if ($gete) {
 				$nots[] = $gets;
 				$note[] = $gete;
 			}
@@ -451,12 +461,9 @@ class WhenRules extends WhenRuleLexer
 
 	/**
 	GetBlocks:
-	Interpret parent::conds into seconds-blocks covering the interval from
-	@dts to immediately (1-sec) before @dte.
-	@dts: datetime object representing resource-local start of period being
-		processed, not necessarily a midnight
-	@dte: datetime object representing resource-local one-past-end of the period,
-		not necessarily a midnight
+	Interpret parent::conds into seconds-blocks covering the interval @bs..@be-1
+	@bs: UTC timestamp for start of period being processed, not necessarily a midnight
+	@be: corresponding stamp for one-past-end of the period, not necessarily a midnight
 	@timeparms: reference to array of parameters for sun-related time calcs
 	@defaultall: optional boolean, whether to return, if parent::conds is not set,
 	the whole interval as one block instead of empty arrays, default FALSE
@@ -466,15 +473,13 @@ class WhenRules extends WhenRuleLexer
 	BUT both arrays will be empty upon error, or if nothing applies and
 		$defaultall is FALSE
 	*/
-	public function GetBlocks($dts, $dte, &$timeparms, $defaultall=FALSE)
+	public function GetBlocks($bs, $be, &$timeparms, $defaultall=FALSE)
 	{
 		$starts = array();
 		$ends = array();
-		if ($dts < $dte && $this->conds) {
+		if ($bs < $be && $this->conds) {
+			$dtw = new \DateTime('@0',NULL);
 			//stamps for period limit checks
-			$bs = $dts->getTimestamp();
-			$be = $dte->getTimestamp();
-			$dtw = clone $dts;
 			$blocks = new Blocks();
 			//for all inclusion-conditions, add to $starts,$ends
 			foreach ($this->conds as &$cond) {
@@ -557,8 +562,7 @@ class WhenRules extends WhenRuleLexer
 				$blocks->MergeBlocks($starts,$ends);
 			}
 		} elseif ($defaultall) {
-			$bs = $dts->getTimestamp();
-			$be = $dte->getTimestamp() - 1;
+			$be--; //no-longer 1-past
 			$starts[] = min($bs,$be);
 			$ends[] = max($bs,$be);
 		}
@@ -567,11 +571,10 @@ class WhenRules extends WhenRuleLexer
 
 	/**
 	AllIntervals:
-	Get array of pairs of timestamps representing conforming time-blocks in the
-	 interval starting at @dts and ending 1-second before @dte
+	Get pair(s) of timestamps representing time-block(s) in @bs..@be-1 that	conform to @descriptor
 	@descriptor: interval-language string to be interpreted, or some variety of FALSE
-	@dts: datetime object for UTC start (midnight) of 1st day of period being processed
-	@dte: datetime object representing 1-second after the end of the period of interest
+	@bs: UTC timestamp for start of period being processed
+	@be: corresponding stamp for 1-past-end of the period
 	@timeparms: reference to array of parameters from self::TimeParms, used in time calcs
 	@defaultall: optional boolean, whether to return, upon some sort of problem,
 		arrays representing the whole period instead of FALSE, default FALSE
@@ -580,37 +583,35 @@ class WhenRules extends WhenRuleLexer
 	 [1] = array of corresponding stamps for interval-last-seconds (NOT 1-past)
 	 OR FALSE if no descriptor, or parsing fails, and @defaultall is FALSE
 	*/
-	public function AllIntervals($descriptor, $dts, $dte, &$timeparms, $defaultall=FALSE)
+	public function AllIntervals($descriptor, $bs, $be, &$timeparms, $defaultall=FALSE)
 	{
 		if ($descriptor) {
 			if (parent::ParseDescriptor($descriptor)) {
-				return self::GetBlocks($dts,$dte,$timeparms,$defaultall);
+				return self::GetBlocks($bs,$be,$timeparms,$defaultall);
 			}
 		}
 		//nothing to report
 		if ($defaultall) {
 			//limiting timestamps
-			$st = $dts->getTimestamp();
-			$nd = $dte->getTimestamp();
-			return array(array($st),array($nd-1));
+			return array(array($bs),array($be-1));
 		}
 		return FALSE;
 	}
 
 	/**
 	NextInterval:
-	Get pair of timestamps representing the earliest conforming time-block in the
-	 interval starting at @dts and ending 1-second before @dte
+	Get a pair of timestamps representing the earliest time-block in @bs..@be-1
+	and conforming to @descriptor
 	@descriptor: interval-language string to be interpreted, or some variety of FALSE
-	@dts: datetime object for UTC start (midnight) of 1st day of period being processed
-	@dte: datetime object representing 1-second after the end of the period of interest
+	@bs: UTC timestamp for start (midnight) of 1st day of period being processed
+	@be: corresponding stamp for 1-past-end of the period of interest
 	@timeparms: reference to array of parameters from self::TimeParms, used in time calcs
 	@slotlen: length (seconds) of wanted block
 	Returns: array with 2 timestamps, or FALSE
 	*/
-	public function NextInterval($descriptor, $dts, $dte, &$timeparms, $slotlen)
+	public function NextInterval($descriptor, $bs, $be, &$timeparms, $slotlen)
 	{
-		$res = self::AllIntervals($descriptor,$dts,$dte,$timeparms,TRUE);
+		$res = self::AllIntervals($descriptor,$bs,$be,$timeparms,TRUE);
 		if ($res) {
 			list($starts,$ends) = $res;
 			foreach ($starts as $i->$st) {
@@ -622,32 +623,28 @@ class WhenRules extends WhenRuleLexer
 			return FALSE;
 		}
 		//limiting timestamps
-		$st = $dts->getTimestamp();
-		$nd = $dte->getTimestamp();
-		if ($st+$slotlen <= $nd)
-			return array($st,$st+$slotlen);
+		if ($bs+$slotlen <= $be)
+			return array($bs,$bs+$slotlen);
 		return FALSE;
 	}
 
 	/**
 	IntervalComplies:
-	Determine whether the time-block starting at @dts and ending 1-second
-	 before @dte is consistent with @descriptor
-	@descriptor: interval-language string to be interpreted, or some variety of FALSE
-	@dts: datetime object for UTC start (midnight) of 1st day of period being processed
-	@dte: datetime object representing 1-second after the end of the period of interest
+	Determine whether the whole block @bs..@be-1 is consistent with @descriptor
+	@descriptor: when-rule string to be interpreted, or some variety of FALSE
+	@bs: UTC timestamp for start of the period to be checked
+	@be: corresponding stamp for 1-past-end of the period
 	@timeparms: reference to array of parameters from self::TimeParms, used in time calcs
 	Returns: boolean representing compliance, or TRUE if @descriptor is FALSE,
 		or FALSE if @descriptor is not parsable
 	*/
-	public function IntervalComplies($descriptor, $dts, $dte, &$timeparms)
+	public function IntervalComplies($descriptor, $bs, $be, &$timeparms)
 	{
-		$res = self::AllIntervals($descriptor,$dts,$dte,$timeparms,TRUE);
+		$res = self::AllIntervals($descriptor,$bs,$be,$timeparms,TRUE);
 		if ($res) {
 			$blocks = new Blocks();
 			list($starts,$ends) = $blocks->DiffBlocks(
-				array($dts->getTimestamp()),array($dte->getTimestamp()), //TODO off-by-1 ?
-				$res[0],$res[1]);
+				array($bs),array($be-1),$res[0],$res[1]);
 			return (count($starts) == 0); //none of the interval is not covered by $descriptor
 		}
 		return FALSE;
