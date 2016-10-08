56b2049197c6cc9bdbc7a5a0825c6c07fee06755
tom2tom <tpgww@onepost.net>
2016-10-08 22:16:48 +1100

    first working version

--- lib/class.Cleanold_task.php
+++ lib/class.Cleanold_task.php
@@ -24,36 +24,40 @@ class Cleanold_task implements \CmsRegularTask
 
 	public function test($time='')
 	{
-/* TODO
 		$mod = \cms_utils::get_module(self::MODNAME);
-		if (!($mod->GetPreference('logsends')
-		  || $mod->GetPreference('logdeliveries')))
-			return FALSE;
-		$days = (int)$mod->GetPreference('logdays');
-		if ($days <= 0)
-			return FALSE;
-		if (!$time)
-			$time = time();
-		$last_cleared = $mod->GetPreference('datalastclear',0);
-		return ($time >= $last_cleared + $days*86400);
-*/
-		return FALSE;
+		$sql = 'SELECT 1 AS gone FROM '.$mod->DataTable.' WHERE active=0 UNION SELECT 1 AS gone FROM '.$mod->RepeatTable.' WHERE active=0';
+		$res = $mod->dbHandle->GetOne($sql);
+		return ($res != FALSE);
 	}
 
 	public function execute($time='')
 	{
-		if (!$time)
-			$time = time();
-//TODO	\smsg_utils::clean_log(NULL,$time);
+		$mod = \cms_utils::get_module(self::MODNAME);
+		$sql = <<<EOS
+SELECT D.bkg_id,I.item_id,I.timezone FROM $mod->DataTable D
+JOIN $mod->ItemTable I ON D.item_id=I.item_id
+WHERE D.active=0
+EOS;
+		$rows = $mod->dbHandle->GetArray($sql);
+		if ($rows) {
+			$sql = 'DELETE FROM '.$mod->DataTable.' WHERE bkg_id=';
+			$utils = new Utils();
+			foreach ($rows as $one) {
+				$st = $utils->GetZoneTime($one['timezone']);
+				$base = PHP_INT_MAX - 10000000; //TODO;
+				$len = $utils->GetInterval($mod,$one['item_id'],'keep',0);
+				if (0) { //$st > $base + $len) {
+					$mod->dbHandle->Execute($sql.$one['bkg_id']);
+				}
+			}
+		}
+		//TODO also $mod->RepeatTable, && maybe $mod->DataTable rows for that bulk_id
+		//TODO also $mod->HistoryTable
 		return TRUE;
 	}
 
 	public function on_success($time='')
 	{
-		if (!$time)
-			$time = time();
-		$mod = \cms_utils::get_module(self::MODNAME);
-		$mod->SetPreference('datalastclear',$time);
 	}
 
 	public function on_failure($time='')
