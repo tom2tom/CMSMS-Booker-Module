a001ceffbf592dab3f54dbd091a8a26c352351c1
tom2tom <tpgww@onepost.net>
2016-10-13 08:15:12 +1100

    8-byte integer fields add removed field to HistoryTable

--- method.install.php
+++ method.install.php
@@ -11,96 +11,96 @@ $taboptarray = array('mysql' => 'ENGINE MyISAM CHARACTER SET utf8 COLLATE utf8_g
 $dict = NewDataDictionary($db);
 $pre = cms_db_prefix();
 /*
- items (i.e. groups and resources) table schema:
- NOTE (almost) no NOTNULL/default values, so inheritance can be determined
- NOTE: changes here must be reflected in action.openitem, Import::ImportItems(), Export::ExportItems()
-	item_id:
-	alias:
-	name:
-	description:
-	keywords: comma-separated descriptors/tags for similarity scans
-	pickname: for a group - name to display in resource-picker
-	membersname: for a group - generic plural member-descriptor e.g. 'members'
-	image: filename or ,-separated series of names, uploaded file(s)
-	available: interval-descriptor string or empty for always-available
-	pickthis: whether to display this item in resource-picker
- 	pickmembers: for a group - whether to display its members in resource-picker
-	slottype: enumerator for interval of a single booking per TimeIntervals()
-	slotcount: count of slottype intervals which, together with slottype, defines length of a bookings
-	bookcount: max. slots per booking, 0 = no limit, 1 = no need for booking end choice
-	bookertell: whether to (try to) send confirmation message to booker, default inherit
- 	leadtype: enumerator for interval used for max. lead-time for bookings
-	leadcount: count of leadtype intervals which, together with leadtype, defines max. lead-time for (no-repeat) bookings
-	rationcount: max. pending bookings for any specific booker
-	keeptype: enumerator for interval used for max. retention-time for past bookings
-	keepcount: count of keeptype intervals which, together with keeptype, defines max. retention-time for bookings history
-	grossfees: whether recorded fees include sales tax, default true
-	taxrate: sales tax rate, 0..1 assumed to be a proportion, >=1 assumed percent
- 	latitude: for sun-related calcs, accurate to ~1km
-	longitude: ditto
-	timezone: for date/time offsets & calcs
-	dateformat: how to display dates
-	timeformat: how to display times
-	listformat: Booker::LIST* enum representing default layout for list-style bookings display
-	stylesfile: specific (uploaded) css file
-	approver: identifier of whoever handles booking requests for this item
-	approvercontact: contact info for the approver
-	approvertell: whether to (try to) send notice to approver about submitted/recorded booking, default inherit
-	smsprefix: country-code to be prepended to sms messages when needed
-	smspattern: regex for determining whether approvercontact is a phone suitable for sms messages (no whitespace, before country-prefix)
-	formiface: for future use e.g. module name, properties to use, for custom booking-request form
-	paymentiface: for future use e.g. module name, gateway name, properties to use
-	feugroup: id of group of whose members are authorised to make 'direct' (un-mediated) bookings
-	owner: uid of the contact-person for the item, or 0 if there's no such person
-	cleargroup: boolean whether to clear data for item when (sole) parent-group data are cleared, default false
-	subgrpalloc: Booker::ALLOC* enum for sub-group allocation protocol
-	subgrpdata: internal use, for working with subgrpalloc
-	active: enum: 0 never; 1 always; 2 inherit (see also: available); -1 deletion pending while historical data needed
+items (i.e. groups and resources) table schema:
+NOTE (almost) no NOTNULL/default values, so inheritance can be determined
+NOTE: changes here must be reflected in action.openitem, Import::ImportItems(), Export::ExportItems()
+ item_id:
+ alias:
+ name:
+ description:
+ keywords: comma-separated descriptors/tags for similarity scans
+ pickname: for a group - name to display in resource-picker
+ membersname: for a group - generic plural member-descriptor e.g. 'members'
+ image: filename or ,-separated series of names, uploaded file(s)
+ available: interval-descriptor string or empty for always-available
+ pickthis: whether to display this item in resource-picker
+ pickmembers: for a group - whether to display its members in resource-picker
+ slottype: enumerator for interval of a single booking per TimeIntervals()
+ slotcount: count of slottype intervals which, together with slottype, defines length of a bookings
+ bookcount: max. slots per booking, 0 = no limit, 1 = no need for booking end choice
+ bookertell: whether to (try to) send confirmation message to booker, default inherit
+ leadtype: enumerator for interval used for max. lead-time for bookings
+ leadcount: count of leadtype intervals which, together with leadtype, defines max. lead-time for (no-repeat) bookings
+ rationcount: max. pending bookings for any specific booker
+ keeptype: enumerator for interval used for max. retention-time for past bookings
+ keepcount: count of keeptype intervals which, together with keeptype, defines max. retention-time for bookings history
+ grossfees: whether recorded fees include sales tax, default true
+ taxrate: sales tax rate, 0..1 assumed to be a proportion, >=1 assumed percent
+ latitude: for sun-related calcs, accurate to ~1km
+ longitude: ditto
+ timezone: for date/time offsets & calcs
+ dateformat: how to display dates
+ timeformat: how to display times
+ listformat: Booker::LIST* enum representing default layout for list-style bookings display
+ stylesfile: specific (uploaded) css file
+ approver: identifier of whoever handles booking requests for this item
+ approvercontact: contact info for the approver
+ approvertell: whether to (try to) send notice to approver about submitted/recorded booking, default inherit
+ smsprefix: country-code to be prepended to sms messages when needed
+ smspattern: regex for determining whether approvercontact is a phone suitable for sms messages (no whitespace, before country-prefix)
+ formiface: for future use e.g. module name, properties to use, for custom booking-request form
+ paymentiface: for future use e.g. module name, gateway name, properties to use
+ feugroup: id of group of whose members are authorised to make 'direct' (un-mediated) bookings
+ owner: uid of the contact-person for the item, or 0 if there's no such person
+ cleargroup: boolean whether to clear data for item when (sole) parent-group data are cleared, default false
+ subgrpalloc: Booker::ALLOC* enum for sub-group allocation protocol
+ subgrpdata: internal use, for working with subgrpalloc
+ active: enum: 0 never; 1 always; 2 inherit (see also: available); -1 deletion pending while historical data needed
 */
-$fields = "
- item_id I(4) KEY,
- alias C(24),
- name C(64),
- description X,
- keywords C(256),
- pickname C(48),
- membersname C(32),
- image C(128),
- available C(128),
- pickthis I(1) DEFAULT 1,
- pickmembers I(1) DEFAULT 0,
- slottype I(1) DEFAULT 1,
- slotcount I(1) DEFAULT 0,
- leadtype I(1) DEFAULT 2,
- leadcount I(1) DEFAULT 0,
- keeptype I(1) DEFAULT 5,
- keepcount I(1) DEFAULT 0,
- bookcount I(1) DEFAULT 0,
- rationcount I(1) DEFAULT 0,
- grossfees I(1) DEFAULT 1,
- taxrate N(8.4),
- latitude N(8.3),
- longitude N(8.3),
- timezone C(48),
- dateformat C(12),
- timeformat C(12),
- listformat I(1),
- stylesfile C(36),
- approver C(64),
- approvercontact C(128),
- approvertell I(1) DEFAULT 1,
- bookertell I(1) DEFAULT 1,
- smsprefix C(8),
- smspattern C(32),
- formiface C(48),
- paymentiface C(48),
- feugroup I,
- owner I,
- cleargroup I(1) DEFAULT 0,
- subgrpalloc I(1),
- subgrpdata I(1) DEFAULT 0,
- active I(1) DEFAULT 1
-";
+$fields = '
+item_id I(4) KEY,
+alias C(24),
+name C(64),
+description X,
+keywords C(256),
+pickname C(48),
+membersname C(32),
+image C(128),
+available C(128),
+pickthis I(1) DEFAULT 1,
+pickmembers I(1) DEFAULT 0,
+slottype I(1) DEFAULT 1,
+slotcount I(1) DEFAULT 0,
+leadtype I(1) DEFAULT 2,
+leadcount I(1) DEFAULT 0,
+keeptype I(1) DEFAULT 5,
+keepcount I(1) DEFAULT 0,
+bookcount I(1) DEFAULT 0,
+rationcount I(1) DEFAULT 0,
+grossfees I(1) DEFAULT 1,
+taxrate N(8.4),
+latitude N(8.3),
+longitude N(8.3),
+timezone C(48),
+dateformat C(12),
+timeformat C(12),
+listformat I(1),
+stylesfile C(36),
+approver C(64),
+approvercontact C(128),
+approvertell I(1) DEFAULT 1,
+bookertell I(1) DEFAULT 1,
+smsprefix C(8),
+smspattern C(32),
+formiface C(48),
+paymentiface C(48),
+feugroup I(8),
+owner I(8),
+cleargroup I(1) DEFAULT 0,
+subgrpalloc I(1),
+subgrpdata I(1) DEFAULT 0,
+active I(1) DEFAULT 1
+';
 $sqlarray = $dict->CreateTableSQL($this->ItemTable, $fields, $taboptarray);
 if ($sqlarray == FALSE)
 	return FALSE;
@@ -118,20 +118,20 @@ $sql = 'INSERT INTO '.$this->ItemTable.' (item_id,name) VALUES (?,?)'; //TODO mo
 $db->Execute($sql,array($gid,$this->Lang('groupdefault')));
 
 /*
- group-relationships table schema:
+group-relationships table schema:
  gid: unique identifier
  child: id of resource or group, indexed, not unique
  parent: id of group of which child is a member
  likeorder: order of children 'likeness' in parent
  proximity: order of multi-parents in all of which child is a member
 */
-$fields = "
- gid I AUTO KEY,
- child I(2) NOTNULL DEFAULT 0,
- parent I(2) NOTNULL DEFAULT 0,
- likeorder I(2) NOTNULL DEFAULT 1,
- proximity I(2) NOTNULL DEFAULT 1
-";
+$fields = '
+gid I AUTO KEY,
+child I(2) NOTNULL DEFAULT 0,
+parent I(2) NOTNULL DEFAULT 0,
+likeorder I(2) NOTNULL DEFAULT 1,
+proximity I(2) NOTNULL DEFAULT 1
+';
 $sqlarray = $dict->CreateTableSQL($this->GroupTable, $fields, $taboptarray);
 if ($sqlarray == FALSE)
 	return FALSE;
@@ -155,17 +155,17 @@ non-repeated bookings-data table schema:
  active: enum/boolean 1, or 0 if booking has been deleted but historic data remain
 Booker\CSV::ImportBookings must conform to this
 */
-$fields = "
- bkg_id I(4) KEY,
- bulk_id I(4) DEFAULT 0,
- item_id I(4),
- slotstart I,
- slotlen I(4),
- booker_id I(4),
- status I(1) DEFAULT ".Booker::STATNONE.",
- paid I(1) DEFAULT 0,
- active I(1) DEFAULT 1
-";
+$fields = '
+bkg_id I(4) KEY,
+bulk_id I(4) DEFAULT 0,
+item_id I(4),
+slotstart I(8),
+slotlen I(4),
+booker_id I(4),
+status I(1) DEFAULT '.Booker::STATNONE.',
+paid I(1) DEFAULT 0,
+active I(1) DEFAULT 1
+';
 $sqlarray = $dict->CreateTableSQL($this->DataTable, $fields, $taboptarray);
 if ($sqlarray == FALSE)
 	return FALSE;
@@ -190,17 +190,17 @@ repeated bookings-data table schema:
  paid: boolean
  active: enum/boolean 1, or 0 if booking has been deleted but historic data remain
 */
-$fields = "
- bkg_id I(4) KEY,
- item_id I(4),
- formula C(256),
- booker_id I(4),
- checkedfrom I DEFAULT 0,
- checkedto I DEFAULT 0,
- subgrpcount I(1) DEFAULT 1,
- paid I(1) DEFAULT 0,
- active I(1) DEFAULT 1
-";
+$fields = '
+bkg_id I(4) KEY,
+item_id I(4),
+formula C(256),
+booker_id I(4),
+checkedfrom I DEFAULT 0,
+checkedto I DEFAULT 0,
+subgrpcount I(1) DEFAULT 1,
+paid I(1) DEFAULT 0,
+active I(1) DEFAULT 1
+';
 $sqlarray = $dict->CreateTableSQL($this->RepeatTable,$fields,$taboptarray);
 if ($sqlarray == FALSE)
 	return FALSE;
@@ -216,13 +216,13 @@ resource-availabilty-cache table schema:
  slotlen: seconds available
  cond_id: condition identifier
 */
-/*$fields = "
- avl_id I(4) AUTO KEY,
- item_id I(4),
- slotstart I,
- slotlen I(4),
- cond_id I(4)
-";
+/*$fields = '
+avl_id I(4) AUTO KEY,
+item_id I(4),
+slotstart I(8),
+slotlen I(4),
+cond_id I(4)
+';
 $sqlarray = $dict->CreateTableSQL($this->AvailTable,$fields,$taboptarray);
 if ($sqlarray == FALSE)
 	return FALSE;
@@ -236,7 +236,7 @@ $dict->ExecuteSQLArray($sqlarray);
 //$db->CreateSequence($this->AvailTable.'_seq');
 */
 /*
- Fees for resource usage & related conditions
+Fees for resource usage & related conditions
  condition_id:
  item_id: group/item to which the condition applies
  signature: identifier for cross-resource matching, raw crc32 hash of slottype.slotcount.fee.feecondition
@@ -248,22 +248,22 @@ $dict->ExecuteSQLArray($sqlarray);
  usercondition: string, numeric usergroup 0..9 or comma-seprated series of them or empty
  condorder: enum for display-order and fee-application-order
  active: boolean/enum whether to use this fee
- NOTE changes to this field-structure must be replicated in the add-fee mechanism
- in action.fees.php
+NOTE changes to this field-structure must be replicated in the add-fee mechanism
+in action.fees.php
 */
-$fields = "
- condition_id I(4) KEY,
- item_id I(4),
- signature I(4),
- description C(64),
- slottype I(1),
- slotcount I(1),
- fee N(8.2),
- feecondition C(128),
- usercondition C(32),
- condorder I(1) DEFAULT -1,
- active I(1) DEFAULT 1
-";
+$fields = '
+condition_id I(4) KEY,
+item_id I(4),
+signature I(4),
+description C(64),
+slottype I(1),
+slotcount I(1),
+fee N(8.2),
+feecondition C(128),
+usercondition C(32),
+condorder I(1) DEFAULT -1,
+active I(1) DEFAULT 1
+';
 $sqlarray = $dict->CreateTableSQL($this->FeeTable,$fields,$taboptarray);
 if ($sqlarray == FALSE)
 	return FALSE;
@@ -273,7 +273,7 @@ if ($res != 2)
 $db->CreateSequence($this->FeeTable.'_seq');
 
 /*
- bookers table schema:
+bookers table schema:
  booker_id: table key
  name: identifier for display, and identity check if publicid N/A
  publicid: login/account for 'registed' bookers
@@ -285,18 +285,18 @@ $db->CreateSequence($this->FeeTable.'_seq');
  displayclass: display-stying enum 1..Booker::USERSTYLES
  active: whether currently enabled
 */
-$fields = "
- booker_id I(4) KEY,
- name C(64),
- publicid C(32),
- passhash C(48),
- address C(96),
- phone C(24),
- addwhen I,
- type I(1) DEFAULT 0,
- displayclass I(1) DEFAULT 1,
- active I(1) DEFAULT 1
-";
+$fields = '
+booker_id I(4) KEY,
+name C(64),
+publicid C(32),
+passhash C(48),
+address C(96),
+phone C(24),
+addwhen I(8),
+type I(1) DEFAULT 0,
+displayclass I(1) DEFAULT 1,
+active I(1) DEFAULT 1
+';
 $sqlarray = $dict->CreateTableSQL($this->BookerTable,$fields,$taboptarray);
 if ($sqlarray == FALSE)
 	return FALSE;
@@ -306,7 +306,7 @@ if ($res != 2)
 $db->CreateSequence($this->BookerTable.'_seq');
 
 /*
- NOTE action.requestbooking.php must be conformed to any change here
+NOTE action.requestbooking.php must be conformed to any change here
  history table schema:
  history_id: table key
  booker_id: cross-referencer (indexed)
@@ -314,6 +314,7 @@ $db->CreateSequence($this->BookerTable.'_seq');
  subgrpcount: no. of requested items in a group, irrelevant for non-groups
  lodged: UTC timestamp booking submitted/recorded
  approved: UTC timestamp - sometimes needed
+ removed: UTC timestamp - sometimes needed for cleanups
  slotstart: UTC timestamp start of booking
  slotlen: booking length (seconds)
  comment: as supplied by booker as part of request
@@ -325,23 +326,24 @@ $db->CreateSequence($this->BookerTable.'_seq');
  gatetransaction: transaction id reported by payment gateway
  gatedata: json data reported by payment gateway, encrypted
 */
-$fields = "
- history_id I(4) KEY,
- booker_id I(4),
- item_id I(4),
- subgrpcount I(1) DEFAULT 1,
- lodged I,
- approved I,
- slotstart I,
- slotlen I(4),
- comment C(64),
- fee N(8.2),
- netfee N(8.2),
- status I(1) DEFAULT ".Booker::STATNONE.",
- payment I(1) DEFAULT ".Booker::STATFREE.",
- gatetransaction C(48),
- gatedata B
-";
+$fields = '
+history_id I(4) KEY,
+booker_id I(4),
+item_id I(4),
+subgrpcount I(1) DEFAULT 1,
+lodged I(8),
+approved I(8),
+removed I(8),
+slotstart I(8),
+slotlen I(4),
+comment C(64),
+fee N(8.2),
+netfee N(8.2),
+status I(1) DEFAULT '.Booker::STATNONE.',
+payment I(1) DEFAULT '.Booker::STATFREE.',
+gatetransaction C(48),
+gatedata B
+';
 $sqlarray = $dict->CreateTableSQL($this->HistoryTable,$fields,$taboptarray);
 if ($sqlarray == FALSE)
 	return FALSE;
@@ -354,7 +356,7 @@ $dict->ExecuteSQLArray($sqlarray);
 $db->CreateSequence($this->HistoryTable.'_seq');
 
 /*
- item-pickability table schema:
+item-pickability table schema:
  gid: unique identifier
  item_id: group id
  pick: boolean whether to always show @item_id in relevant resource-picklists
@@ -364,14 +366,14 @@ $db->CreateSequence($this->HistoryTable.'_seq');
  pickmembersfor: group identifier or NULL, if not pickmembers, then show members
 	of item_id in relevant resource-picklists when this field's value is an ancestor of item_id
 */
-/*$fields = "
- gid I AUTO KEY,
- item_id I(4) NOTNULL,
- pick I(1) DEFAULT 1,
- pickfor I(4),
- pickmembers I(1) DEFAULT 1,
- pickmembersfor I(4)
-";
+/*$fields = '
+gid I AUTO KEY,
+item_id I(4) NOTNULL,
+pick I(1) DEFAULT 1,
+pickfor I(4),
+pickmembers I(1) DEFAULT 1,
+pickmembersfor I(4)
+';
 $sqlarray = $dict->CreateTableSQL($this->ItemTable.'pick', $fields, $taboptarray);
 if ($sqlarray == FALSE)
 	return FALSE;
@@ -387,13 +389,13 @@ Data cache
  savetime: timestamp - UTC?
  lifetime: seconds
 */
-$fields = "
- cache_id I(2) AUTO KEY,
- keyword C(48),
- value B,
- savetime I,
- lifetime I(4)
-";
+$fields = '
+cache_id I(2) AUTO KEY,
+keyword C(48),
+value B,
+savetime I(8),
+lifetime I(4)
+';
 $sqlarray = $dict->CreateTableSQL($pre.'module_bkr_cache',$fields,$taboptarray);
 $dict->ExecuteSQLArray($sqlarray);
 //this is not for table-data content
