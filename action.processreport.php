7e2a61637487ef8084ddead5201b9a3dec4c9329
tom2tom <tpgww@onepost.net>
2017-06-15 10:30:03 +1000

    renamed file

--- action.processreport.php
+++ action.processreport.php
@@ -7,25 +7,122 @@
 # See file Booker.module.php for full details of copyright, licence, etc.
 #----------------------------------------------------------------------
 
+/*first-pass $params[]
+'display' => string
+OR
+'export' => string
+'task'=> string
+'active_tab' => string
+'resume' => string
+later
+'close' => string
+OR
+'export' => string
+OR
+'range' => string
+'task'=> string
+'startchooser' => string
+'endchooser'=> string
+*/
+
 if (isset($params['close'])) {
+//	$resume = array_pop($params['resume']);
+	$this->Redirect($id,'defaultadmin','',array('active_tab'=>$params['active_tab']));
+}
+//report type
+/* selection-flags
+Booker\Dataops::BKGFUTURE
+Booker\Dataops::BKGPAST
+Booker\Dataops::BKGFREE
+Booker\Dataops::BKGUNPAID
+Booker\Dataops::BKGPAID
+Booker\Dataops::BKGONCE
+Booker\Dataops::BKGREPT
+Booker\Dataops::BKGSINGL
+Booker\Dataops::BKGGROUP
+Booker\Dataops::BKGITEM
+Booker\Dataops::BKGUSER
+Booker\Dataops::BKGUTYPE
+*/
+$t1 = substr($params['task'], 0, 3); //{'itm' bkr' 'rng'}
+$t2 = substr($params['task'], 3); //{'view' 'pay' 'stat'}
+/*switch ($t1) {
+ case 'itm':
+	switch ($t2) {
+	 case 'view':
+		$flags = Booker\Dataops::BKGALL;
+		break;
+	 case 'pay':
+		$flags = Booker\Dataops::BKGALL;
+		break;
+	 case 'stat':
+		$flags = Booker\Dataops::BKGALL;
+		break;
+	 default:
+		$flags = 0;
+		break;
+	}
+	break;
+ case 'bkr':
+	switch ($t2) {
+	 case 'view':
+		$flags = Booker\Dataops::BKGALL;
+		break;
+	 case 'pay':
+		$flags = Booker\Dataops::BKGALL;
+		break;
+	 case 'stat':
+		$flags = Booker\Dataops::BKGALL;
+		break;
+	 default:
+		$flags = Booker\Dataops::BKGALL;
+		break;
+	}
+	break;
+ case 'rng':
+	switch ($t2) {
+	 case 'view':
+		$flags = Booker\Dataops::BKGALL;
+		break;
+	 case 'pay':
+		$flags = Booker\Dataops::BKGALL;
+		break;
+	 case 'stat':
+		$flags = Booker\Dataops::BKGALL;
+		break;
+	 default:
+		$flags = Booker\Dataops::BKGALL;
+		break;
+	}
+	break;
+ default:
+	$flags = Booker\Dataops::BKGALL;
+	break;
+}
+*/
 
-} elseif (isset($params['export'])) {
+if (isset($params['range'])) {
+//TODO process $params['startchooser', 'endchooser']
+}
 
+$funcs = new Booker\Dataops();
+$data = $funcs->FilterData($this, Booker\Dataops::BKGALL); //, $itemid = FALSE, $userid = FALSE, $typeid = FALSE);
+
+if (isset($params['export'])) {
+	//TODO STUFF
+	exit;
 }
 
 $tplvars = array();
 
 $this->_BuildNav($id, $returnid, $params, $tplvars);
-$tplvars['startform'] = $this->CreateFormStart($id, 'report', $returnid, 'POST', '', '', '',
-	array(TODO));
+$tplvars['startform'] = $this->CreateFormStart($id, 'processreport', $returnid, 'POST', '', '', '',
+	array(task=>$params['task'],'active_tab'=>$params['active_tab'],'resume'=>$params['resume']));
 $tplvars['endform'] = $this->CreateFormEnd();
 
 if (!empty($params['message'])) {
 	$tplvars['message'] = $params['message'];
 }
-
-$funcs = new Booker\Dataops();
-$data = $funcs->FilterData($this, $flags, $itemid = FALSE, $userid = FALSE, $typeid = FALSE);
 
 $rows = array();
 if ($data) {
@@ -35,20 +132,34 @@ if ($data) {
 	$jsloads = array();
 	$baseurl = $this->GetModuleURLPath();
 
-	$tplvars['startchooser'] = $this->CreateInput();
-	$tplvars['endchooser'] = $this->CreateInput();
-	//TODO
+	$tplvars['startchooser'] = NULL; //$this->CreateInput();
+	$tplvars['endchooser'] = NULL; //$this->CreateInput();
+	//TODO strings
 	$tplvars['range'] = $this->CreateInputSubmit($id, 'range', $this->Lang('export'),
 		'title="'.$this->Lang('tip_export_selected_records').
-		'" onclick="return any_selected();"');
-
+		'" onclick="TODO"');
+/* each row of $data
+'name'	string	"Coaching" or NULL
+'publicid' string or NULL
+'address'	string	"TBA" or NULL
+'phone' string or NULL
+'what'	string	"Court 5"
+'slotstart'	string	"1497538800"
+'slotlen'	string	"14399"
+'status'	string	"10"
+'fee'	string	"0.00"
+'feepaid'	string	"0.00"
+'payment'	string	"40"
+*/
+	//$colnames = func($t1,$t2)
 	$colnames = array(
 		$this->Lang('title_X'),
 		$this->Lang('title_X'),
 		$this->Lang('title_X')
 	);
-	$colsorts = array(1,1,1);
 	$tplvars['colnames'] = $colnames;
+
+	$colsorts = array_fill(0, count($colnames), 1); //all sortable
 	$tplvars['colsorts'] = $colsorts;
 
 	$theme = ($this->before20) ? cmsms()->get_variable('admintheme'):
