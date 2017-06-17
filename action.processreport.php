73b3971b2087669bd36e3fb92b35041aebc8d4f3
tom2tom <tpgww@onepost.net>
2017-06-17 23:55:58 +1000

    first working version

--- action.processreport.php
+++ action.processreport.php
@@ -2,7 +2,7 @@
 #----------------------------------------------------------------------
 # Module: Booker - a resource booking module
 # Action: report
-# Display bookings information
+# Display bookings summary-information
 #----------------------------------------------------------------------
 # See file Booker.module.php for full details of copyright, licence, etc.
 #----------------------------------------------------------------------
@@ -25,183 +25,149 @@ OR
 'endchooser'=> string
 */
 
+$utils = new Booker\Utils();
+$utils->DecodeParameters($params);
+
 if (isset($params['close'])) {
 //	$resume = array_pop($params['resume']);
 	$this->Redirect($id,'defaultadmin','',array('active_tab'=>$params['active_tab']));
 }
+
+$params['task'] = 'itmview'; //DEBUG
 //report type
-/* selection-flags
-Booker\Dataops::BKGFUTURE
-Booker\Dataops::BKGPAST
-Booker\Dataops::BKGFREE
-Booker\Dataops::BKGUNPAID
-Booker\Dataops::BKGPAID
-Booker\Dataops::BKGONCE
-Booker\Dataops::BKGREPT
-Booker\Dataops::BKGSINGL
-Booker\Dataops::BKGGROUP
-Booker\Dataops::BKGITEM
-Booker\Dataops::BKGUSER
-Booker\Dataops::BKGUTYPE
-*/
-$t1 = substr($params['task'], 0, 3); //{'itm' bkr' 'rng'}
-$t2 = substr($params['task'], 3); //{'view' 'pay' 'stat'}
-/*switch ($t1) {
- case 'itm':
-	switch ($t2) {
-	 case 'view':
-		$flags = Booker\Dataops::BKGALL;
-		break;
-	 case 'pay':
-		$flags = Booker\Dataops::BKGALL;
-		break;
-	 case 'stat':
-		$flags = Booker\Dataops::BKGALL;
-		break;
-	 default:
-		$flags = 0;
-		break;
-	}
+switch ($params['task']) {
+ case 'itmview':
+	$t = 'item-summary';
 	break;
- case 'bkr':
-	switch ($t2) {
-	 case 'view':
-		$flags = Booker\Dataops::BKGALL;
-		break;
-	 case 'pay':
-		$flags = Booker\Dataops::BKGALL;
-		break;
-	 case 'stat':
-		$flags = Booker\Dataops::BKGALL;
-		break;
-	 default:
-		$flags = Booker\Dataops::BKGALL;
-		break;
-	}
+ case 'itmpay':
+	$t = 'item-payments';
 	break;
- case 'rng':
-	switch ($t2) {
-	 case 'view':
-		$flags = Booker\Dataops::BKGALL;
-		break;
-	 case 'pay':
-		$flags = Booker\Dataops::BKGALL;
-		break;
-	 case 'stat':
-		$flags = Booker\Dataops::BKGALL;
-		break;
-	 default:
-		$flags = Booker\Dataops::BKGALL;
-		break;
-	}
+ case 'itmstat':
+	$t = 'item-status';
+	break;
+ case 'bkrview':
+	$t = 'booker-summary';
+	break;
+ case 'bkrpay':
+	$t = 'booker-payments';
+	break;
+ case 'bkrstat':
+	$t = 'booker-status';
+	break;
+ case 'rngview':
+	$t = 'interval-summary';
+	break;
+ case 'rngpay':
+	$t = 'interval-payments';
+	break;
+ case 'rngstat':
+	$t = 'interval-status';
 	break;
  default:
-	$flags = Booker\Dataops::BKGALL;
-	break;
+	echo $this->Lang('err_system');
+	return;
 }
-*/
 
 if (isset($params['range'])) {
 //TODO process $params['startchooser', 'endchooser']
 }
 
-$funcs = new Booker\Dataops();
-$data = $funcs->FilterData($this, Booker\Dataops::BKGALL); //, $itemid = FALSE, $userid = FALSE, $typeid = FALSE);
+$display = array(); //output populated by included file, or maybe empty
+
+require 'report.'.$t.'.php';
 
 if (isset($params['export'])) {
-	//TODO STUFF
-	exit;
+	if ($display) {
+		//TODO STUFF
+		exit;
+	} else {
+		$params['message'] = $this->Lang('err_data');
+	}
 }
 
 $tplvars = array();
 
-$this->_BuildNav($id, $returnid, $params, $tplvars);
-$tplvars['startform'] = $this->CreateFormStart($id, 'processreport', $returnid, 'POST', '', '', '',
-	array(task=>$params['task'],'active_tab'=>$params['active_tab'],'resume'=>$params['resume']));
+$tplvars['pagenav'] = $utils->BuildNav($this,$id,$returnid,$params['action'],$params);
+$tplvars['startform'] = $this->CreateFormStart($id,'processreport',$returnid,'POST','','','',
+	array('task'=>$params['task'],'active_tab'=>$params['active_tab']));
+//TODO 'resume'=>$params['resume']
 $tplvars['endform'] = $this->CreateFormEnd();
+
+$tplvars['title'] = 'PAGE TITLE GOES HERE';
 
 if (!empty($params['message'])) {
 	$tplvars['message'] = $params['message'];
 }
 
-$rows = array();
-if ($data) {
-	//script accumulators
-	$jsincs = array();
-	$jsfuncs = array();
-	$jsloads = array();
-	$baseurl = $this->GetModuleURLPath();
+//script accumulators
+$jsincs = array();
+$jsfuncs = array();
+$jsloads = array();
+$baseurl = $this->GetModuleURLPath();
 
-	$tplvars['startchooser'] = NULL; //$this->CreateInput();
-	$tplvars['endchooser'] = NULL; //$this->CreateInput();
-	//TODO strings
-	$tplvars['range'] = $this->CreateInputSubmit($id, 'range', $this->Lang('export'),
-		'title="'.$this->Lang('tip_export_selected_records').
-		'" onclick="TODO"');
-/* each row of $data
-'name'	string	"Coaching" or NULL
-'publicid' string or NULL
-'address'	string	"TBA" or NULL
-'phone' string or NULL
-'what'	string	"Court 5"
-'slotstart'	string	"1497538800"
-'slotlen'	string	"14399"
-'status'	string	"10"
-'fee'	string	"0.00"
-'feepaid'	string	"0.00"
-'payment'	string	"40"
-*/
-	//$colnames = func($t1,$t2)
-	$colnames = array(
-		$this->Lang('title_X'),
-		$this->Lang('title_X'),
-		$this->Lang('title_X')
-	);
-	$tplvars['colnames'] = $colnames;
+$dc = count($display);
+$tplvars['dcount'] = $dc;
+if ($dc) {
+	$tplvars['data'] = $display;
+	$tplvars['colnames'] = $coltitles;
 
-	$colsorts = array_fill(0, count($colnames), 1); //all sortable
-	$tplvars['colsorts'] = $colsorts;
+	//TODO titles and help for these
+	//TODO date-pick for these
+	$tplvars['startchooser'] = $this->CreateInputText($id,'startchooser','',30,64); //TODO "ranger" class
+	$tplvars['endchooser'] = $this->CreateInputText($id,'endchooser','',30,64); //ditto
+	//for date-picker
+	$stylers = <<<EOS
+<link rel="stylesheet" type="text/css" href="{$baseurl}/css/pikaday.css" />
+EOS;
+	$jsincs[] = <<<EOS
+<script type="text/javascript" src="{$baseurl}/lib/js/pikaday.min.js"></script>
+<script type="text/javascript" src="{$baseurl}/lib/js/pikaday.jquery.min.js"></script>
+<script type="text/javascript" src="{$baseurl}/lib/js/php-date-formatter.min.js"></script>
+EOS;
+	$prevm = $this->Lang('prevm');
+	$nextm = $this->Lang('nextm');
+	//js wants quoted period-names
+	$t = $this->Lang('longmonths');
+	$mnames = "'".str_replace(",","','",$t)."'";
+	$t = $this->Lang('longdays');
+	$dnames = "'".str_replace(",","','",$t)."'";
+	$t = $this->Lang('shortdays');
+	$sdnames = "'".str_replace(",","','",$t)."'";
+	$jsloads[] = <<<EOS
+ $('.ranger').pikaday({
+  field: document.getElementById('{$id}showfrom'),
+  i18n: {
+   previousMonth: '$prevm',
+   nextMonth: '$nextm',
+   months: [$mnames],
+   weekdays: [$dnames],
+   weekdaysShort: [$sdnames]
+  },
+  onClose: function() {
+   if('_d' in this && this._d) {
+    var fmt = new DateFormatter();
+    var dt = fmt.formatDate(this._d,'Y-m-d');
+    $('#{$id}showfrom').val(dt);
+   }
+  }
+ });
+EOS;
+	$tplvars['range'] = $this->CreateInputSubmit($id,'range',$this->Lang('interval'),
+		'title="'.$this->Lang('tip_report_interval').'" onclick="return TODO();"');
+	//TODO js to validate chooser(s) content
+	if ($dc > 1) {
+		$pagerows = $this->GetPreference('pagerows',10);
+		$paged = ($pagerows > 5 && $dc > $pagerows);
 
-	$theme = ($this->before20) ? cmsms()->get_variable('admintheme'):
-		cms_utils::get_theme_object();
-	$icon_view = $theme->DisplayImage('icons/system/view.gif', $this->Lang('view'), '', '', 'systemicon');
-	$icon_export = $theme->DisplayImage('icons/system/export.gif', $this->Lang('export'), '', '', 'systemicon');
-
-	foreach ($data as &$one) {
-		$fields = array();
-		$rid = (int)$one['record_id'];
-		$oneset = new stdClass();
-		ksort($fields); //conform order to titles
-		$oneset->fields = $fields;
-		$oneset->view = $this->CreateLink($id, 'browse_record', '', $icon_view,
-			['record_id'=>$rid, 'browser_id'=>$bid, 'form_id'=>$fid]);
-		$oneset->export = $this->CreateLink($id, 'export_record', '', $icon_export,
-			['record_id'=>$rid, 'browser_id'=>$bid, 'form_id'=>$fid]);
-		$oneset->sel = $this->CreateInputCheckbox($id, 'sel[]', $rid, -1);
-		$rows[] = $oneset;
-	}
-	unset($one);
-}
-
-$tplvars['data'] = $rows;
-$rc = count($rows);
-$tplvars['rcount'] = $rc;
-if ($rc) {
-	if ($rc > 1) {
 		$jsincs[] = <<<EOS
 <script type="text/javascript" src="{$baseurl}/lib/js/jquery.metadata.min.js"></script>
 <script type="text/javascript" src="{$baseurl}/lib/js/jquery.SSsort.min.js"></script>
 EOS;
 		$jsloads[] = <<<EOS
  $('#datatable').SSsort({
-  sortClass: 'SortAble',
-  ascClass: 'SortUp',
-  descClass: 'SortDown',
   oddClass: 'row1',
   evenClass: 'row2',
-  oddsortClass: 'row1s',
-  evensortClass: 'row2s',
-  paginate: true,
+  paginate: {$paged},
   pagesize: {$pagerows},
   currentid: 'cpage',
   countid: 'tpage'
@@ -212,40 +178,34 @@ function select_all(cb) {
  $('#datatable > tbody').find('input[type="checkbox"]').attr('checked',cb.checked);
 }
 EOS;
-		$tplvars['header_checkbox'] =
-			$this->CreateInputCheckbox($id, 'selectall', TRUE, FALSE, 'onclick="select_all(this);"');
-	} else {
-		$tplvars['header_checkbox'] = NULL;
-	}
+		if ($paged) {
+			//more setup for SSsort
+			$curpg='<span id="cpage">1</span>';
+			$totpg='<span id="tpage">'.ceil($dc/$pagerows).'</span>';
 
-	if ($pagerows && $rc>$pagerows) {
-		//more setup for SSsort
-		$curpg='<span id="cpage">1</span>';
-		$totpg='<span id="tpage">'.ceil($rc/$pagerows).'</span>';
+			$choices = [strval($pagerows) => $pagerows];
+			$f = ($pagerows < 4) ? 5 : 2;
+			$n = $pagerows * $f;
+			if ($n < $dc) {
+				$choices[strval($n)] = $n;
+			}
+			$n *= 2;
+			if ($n < $dc) {
+				$choices[strval($n)] = $n;
+			}
+			$choices[$this->Lang('all')] = 0;
 
-		$choices = [strval($pagerows) => $pagerows];
-		$f = ($pagerows < 4) ? 5 : 2;
-		$n = $pagerows * $f;
-		if ($n < $rc) {
-			$choices[strval($n)] = $n;
-		}
-		$n *= 2;
-		if ($n < $rc) {
-			$choices[strval($n)] = $n;
-		}
-		$choices[$this->Lang('all')] = 0;
+			$tplvars += [
+				'hasnav'=>1,
+				'first'=>'<a href="javascript:pagefirst()">'.$this->Lang('first').'</a>',
+				'prev'=>'<a href="javascript:pageback()">'.$this->Lang('previous').'</a>',
+				'next'=>'<a href="javascript:pageforw()">'.$this->Lang('next').'</a>',
+				'last'=>'<a href="javascript:pagelast()">'.$this->Lang('last').'</a>',
+				'pageof'=>$this->Lang('pageof', $curpg, $totpg),
+				'rowchanger'=>$this->CreateInputDropdown($id, 'pagerows', $choices, -1, $pagerows, 'onchange="pagerows(this);"').'&nbsp;&nbsp;'.$this->Lang('pagerows')
+			];
 
-		$tplvars += [
-			'hasnav'=>1,
-			'first'=>'<a href="javascript:pagefirst()">'.$this->Lang('first').'</a>',
-			'prev'=>'<a href="javascript:pageback()">'.$this->Lang('previous').'</a>',
-			'next'=>'<a href="javascript:pageforw()">'.$this->Lang('next').'</a>',
-			'last'=>'<a href="javascript:pagelast()">'.$this->Lang('last').'</a>',
-			'pageof'=>$this->Lang('pageof', $curpg, $totpg),
-			'rowchanger'=>$this->CreateInputDropdown($id, 'pagerows', $choices, -1, $pagerows, 'onchange="pagerows(this);"').'&nbsp;&nbsp;'.$this->Lang('pagerows')
-		];
-
-		$jsfuncs[] = <<<'EOS'
+			$jsfuncs[] = <<<'EOS'
 function pagefirst() {
  $.SSsort.movePage($('#submissions')[0],false,true);
 }
@@ -262,11 +222,19 @@ function pagerows(cb) {
  $.SSsort.setCurrent($('#submissions')[0],'pagesize',parseInt(cb.value));
 }
 EOS;
+		} else {
+			$tplvars['hasnav'] = 0;
+		}
+		$tplvars['header_checkbox'] =
+			$this->CreateInputCheckbox($id, 'selectall', TRUE, FALSE, 'onclick="select_all(this);"');
 	} else {
-		$tplvars['hasnav'] = 0;
+		$tplvars['header_checkbox'] = NULL; //TODO into $colnames
 	}
 
-	$jsfuncs[] = <<<EOS
+	if (1) { //TODO relevant test
+		$tplvars['export'] = $this->CreateInputSubmit($id,'export',$this->Lang('export'),
+			'title="'.$this->Lang('tip_export_selected_records').'" onclick="return any_selected();"');
+		$jsfuncs[] = <<<EOS
 function sel_count() {
  var cb = $('input[name="{$id}sel[]"]:checked');
  return cb.length;
@@ -275,16 +243,28 @@ function any_selected() {
  return (sel_count() > 0);
 }
 EOS;
-	if (1) { //TODO test
-		$tplvars['export'] = $this->CreateInputSubmit($id, 'export', $this->Lang('export'),
-		'title="'.$this->Lang('tip_export_selected_records').
-		'" onclick="return any_selected();"');
 	}
 } else {
 	$tplvars['nodata'] = $this->Lang('nodata');
 }
 
 $tplvars['close'] = $this->CreateInputSubmit($id, 'close', $this->Lang('close'));
+
+if (!empty($stylers)) {
+	//heredoc-var newlines are a problem for in-js quoted strings, so ...
+	$stylers = preg_replace('/[\\n\\r]+/','',$stylers);
+	$t = <<<EOS
+var linkadd = '$stylers',
+ \$head = $('head'),
+ \$linklast = \$head.find("link[rel='stylesheet']:last");
+if (\$linklast.length) {
+ \$linklast.after(linkadd);
+} else {
+ \$head.append(linkadd);
+}
+EOS;
+	echo $utils->MergeJS(FALSE,array($t),FALSE);
+}
 
 $jsall = $utils->MergeJS($jsincs,$jsfuncs,$jsloads);
 unset($jsincs);
