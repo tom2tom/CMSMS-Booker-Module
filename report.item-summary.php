2e004e1f090c165203a2ab4d7366d27b3e46eeb4
tom2tom <tpgww@onepost.net>
2017-06-18 19:39:56 +1000

    include line-total for slotlen improve column titles

--- report.item-summary.php
+++ report.item-summary.php
@@ -11,7 +11,7 @@
 
 //TODO support start/end time-limit(s) per $params['startchooser','endchooser'] if relevant
 $sql =<<<EOS
-SELECT item_id,bkg_id,(bulk=0) AS singl,(bulk=1) AS grp,(bulk>=20) AS rept,slotstart,slotlen
+SELECT item_id,1 AS bkg,(bulk=0) AS singl,(bulk=1) AS grp,(bulk>=20) AS rept,slotstart,slotlen
 FROM $this->DispTable
 WHERE displayed>0
 ORDER BY item_id,slotstart
@@ -20,6 +20,7 @@ EOS;
 $data = $db->GetArray($sql);
 
 if ($data) {
+	$iid = $data[0]['item_id']; //for time-interval determination
 	$dt = new DateTime('@0',NULL);
 	$ic = count($data);
 	for ($i = 0; $i < $ic; $i++) {
@@ -32,7 +33,7 @@ if ($data) {
 	//$group = array('month','weekday');
 	$group = 'month';
 	//TODO total slotlen as 'slotsum'
-	$groupvalue = array('slotlen',array('singl','count'),array('grp','count'),array('rept','count'),array('bkg_id','count'));
+	$groupvalue = array(array('singl','count'),array('grp','count'),array('rept','count'),'bkg','slotlen');
 
 	$funcs = new Booker\Pivot1($data, $pivoton, $group, $groupvalue,
 		TRUE, //include a column showing relevant Pivot::TYPE_* const
@@ -43,8 +44,6 @@ if ($data) {
 //		'type' //column-title - use default
 	);
 	$pivoted = $funcs->fetch();
-
-	if ($pivoted) {
 /* gets array with rows like:
 [0]	array[12]
 [item_id]	integer	1
@@ -60,14 +59,31 @@ if ($data) {
 [M5\singl]	integer	0
 [M5\slotlen]	integer	244784
 */
+	unset($data);
+
+	if ($pivoted) {
+		$slen = $utils->GetInterval($this,$iid,'slot');
+		if ($slen <= 86400) {
+			$slen = 3600;
+			$t = $this->Lang('title_hours');
+		} elseif ($slen < 604800) {
+			$slen = 86400;
+			$t = $this->Lang('title_days');
+		} elseif ($slen < 2592000) {
+			$slen = 604800;
+			$t = $this->Lang('title_weeks');
+		} else {
+			$slen = 2592000;
+			$t = $this->Lang('title_months');
+		}
+
 		$translates = array(
-			'item_id'=>$this->Lang('title_name'),
-			'bkg_id'=>'Count', //$this->Lang('total'), //'title_bookings'
-			'singl'=>'Single', //$this->Lang('bkgtype_single'),
+			'bkg'=>$this->Lang('count'),
 			'grp'=>$this->Lang('bkgtype_grouped'),
+			'item_id'=>$this->Lang('title_name'),
 			'rept'=>$this->Lang('bkgtype_repeated'),
-			'slotlen'=>$this->Lang('duration'),
-			'slotsum'=>$this->Lang('durationsum')
+			'singl'=>$this->Lang('bkgtype_single'),
+			'slotlen'=>$t,
 		);
 /*		$names = explode(',',$this->Lang('shortdays'));
 		foreach ($names as $k => $val) {
@@ -78,14 +94,13 @@ if ($data) {
 		foreach ($names as $k => $val) {
 			$translates['M'.$k] = $val;
 		}
-		$translates += $db->GetAssoc('SELECT item_id,name FROM '.$this->ItemTable.' ORDER BY item_id');
 
 		$row = reset($pivoted);
 		//interpet titles, and log row-indices of 'type','item_id',*\'slotlen','slotsum'
 		$k = 0;
 		$is = array();
-		foreach ($row as $t => $val) {
-			$parts = explode('\\',$t);
+		foreach ($row as $t2 => $val) {
+			$parts = explode('\\',$t2);
 			foreach ($parts as &$val) {
 				switch ($val) {
 				 case 'type':
@@ -96,7 +111,6 @@ if ($data) {
 					$ii = $k;
 					break;
 				 case 'slotlen':
-				 case 'slotsum':
 					$is[] = $k;
 					break;
 				}
@@ -110,11 +124,13 @@ if ($data) {
 			}
 			$k++;
 		}
+		$coltitles[] = $this->Lang('total').'<br />'.$t; //for locally-calculated item-total booked time
 
 		$theme = ($this->before20) ? cmsms()->get_variable('admintheme'):
 			cms_utils::get_theme_object();
 		$icon_view = $theme->DisplayImage('icons/system/view.gif', $this->Lang('view'), '', '', 'systemicon');
-
+		$translates = $db->GetAssoc('SELECT item_id,name FROM '.$this->ItemTable.' ORDER BY item_id');
+		//CHECKME fallback for any missing name ?
 		$ic = count($pivoted);
 		$ic--; //DEBUG = skip the dodgy totals row
 		for ($i = 0; $i < $ic; $i++) {
@@ -124,21 +140,17 @@ if ($data) {
 			unset($row[$it]);
 			$iid = $row[$ii];
 			$row[$ii] = $translates[$iid]; //item name
-			//interpret $row[*\'slotlen','slotsum']
+			//interpret *\'slotlen'
+			$sum = 0.0;
 			foreach ($is as $k) {
-				$row[$k] = round(($row[$k]/3600),1); //TODO better format related to slotlen
+				$row[$k] = round(($row[$k]/$slen),1);
+				$sum += $row[$k];
 			}
+			$row[] = $sum;
 			$oneset->fields = $row;
-			if ($more) {
-				$oneset->view = $this->CreateLink($id,'action','',$icon_view,
-					array($iid)); //TODO
-				$oneset->sel = $this->CreateInputCheckbox($id,'sel[]',$iid,-1);
-			} else {
-				$oneset->view = NULL;
-				$oneset->sel = NULL;
-			}
+			$oneset->view = ($more) ? $this->CreateLink($id,'filter','',$icon_view,
+				array('item_id'=>$iid)) : NULL; //TODO $params[]
 			$display[] = $oneset;
 		}
-		$k = 0;
 	}
 }
