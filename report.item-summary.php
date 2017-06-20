b4041f216601fc082813e983b4818af6b026159d
tom2tom <tpgww@onepost.net>
2017-06-20 13:11:53 +1000

    revise table format

--- report.item-summary.php
+++ report.item-summary.php
@@ -9,6 +9,16 @@
 #----------------------------------------------------------------------
 // see https://gonzalo123.com/2010/01/24/pivot-tables-in-php for related examples
 
+$dt = new DateTime('@0',NULL);
+
+$t = $this->Lang('title_item');
+$t2 = $this->Lang('title_overview');
+/*TODO supplement with interval-description if relevant, stamps 'showfrom','showto'
+S to E, S and after, E and before
+c.f. $this->GetPreference('dateformat') or $idata['dateformat']
+*/
+$tplvars['title'] = $this->Lang('report_title',$t,$t2);
+
 //TODO support start/end time-limit(s) per $params['startchooser','endchooser'] if relevant
 $sql =<<<EOS
 SELECT item_id,1 AS bkg,(bulk=0) AS singl,(bulk=1) AS grp,(bulk>=20) AS rept,slotstart,slotlen
@@ -21,7 +31,6 @@ $data = $db->GetArray($sql);
 
 if ($data) {
 	$iid = $data[0]['item_id']; //for time-interval determination
-	$dt = new DateTime('@0',NULL);
 	$ic = count($data);
 	for ($i = 0; $i < $ic; $i++) {
 		$dt->setTimestamp($data[$i]['slotstart']);
@@ -29,36 +38,21 @@ if ($data) {
 		$data[$i]['weekday'] = 'D'.$dt->format('w');
 	}
 
-	$pivoton = 'item_id';
-	//$group = array('month','weekday');
-	$group = 'month';
-	//TODO total slotlen as 'slotsum'
+	$pivoton = array('item_id','month');
+	$group = null;
 	$groupvalue = array(array('singl','count'),array('grp','count'),array('rept','count'),'bkg','slotlen');
+	$total = $this->Lang('total');
+	$subtotal = $this->Lang('subtotal');
 
-	$funcs = new Booker\Pivot1($data, $pivoton, $group, $groupvalue,
-		TRUE, //include a column showing relevant Pivot::TYPE_* const
-		FALSE, //exclude totals per line
-		TRUE, //include totals per pivoted column
-		TRUE, //include total for all table
-		$this->Lang('total') //title
-//		'type' //column-title - use default
+	$funcs = new Booker\Pivot2($data, $pivoton, $group, $groupvalue,
+		TRUE, //include relevant Pivotbase::TYPE_*
+		FALSE, //exclude  per-line subtotals
+		TRUE, //include pivoted-field subtotals
+		TRUE, //include whole-table totals 
+		$total,
+		$subtotal
 	);
 	$pivoted = $funcs->fetch();
-/* gets array with rows like:
-[0]	array[12]
-[item_id]	integer	1
-[type]	integer	0
-[M4\bkg_id]	integer	2
-[M4\grp]	integer	0
-[M4\rept]	integer	2
-[M4\singl]	integer	0
-[M4\slotlen]	integer	28798
-[M5\bkg_id]	integer	16
-[M5\grp]	integer	0
-[M5\rept]	integer	16
-[M5\singl]	integer	0
-[M5\slotlen]	integer	244784
-*/
 	unset($data);
 
 	if ($pivoted) {
@@ -81,40 +75,36 @@ if ($data) {
 			'bkg'=>$this->Lang('count'),
 			'grp'=>$this->Lang('bkgtype_grouped'),
 			'item_id'=>$this->Lang('title_name'),
+			'month'=>$this->Lang('title_month'),
 			'rept'=>$this->Lang('bkgtype_repeated'),
 			'singl'=>$this->Lang('bkgtype_single'),
 			'slotlen'=>$t,
 		);
-/*		$names = explode(',',$this->Lang('shortdays'));
-		foreach ($names as $k => $val) {
-			$translates['D'.$k] = $val;
+		$months = array();
+		foreach (explode(',',$this->Lang('longmonths')) as $k => $val) {
+			$months['M'.$k] = $val;
 		}
-*/
-		$names = explode(',',$this->Lang('longmonths'));
-		foreach ($names as $k => $val) {
-			$translates['M'.$k] = $val;
-		}
+		$translates += $months;
 
 		$row = reset($pivoted);
-		//interpet titles, and log row-indices of 'type','item_id',*\'slotlen','slotsum'
-		$k = 0;
-		$is = array();
+		//interpet titles, and log row-indices of *\'slotlen'
+		$summers = array();
 		foreach ($row as $t2 => $val) {
 			$parts = explode('\\',$t2);
-			foreach ($parts as &$val) {
+			foreach ($parts as $k => &$val) {
 				switch ($val) {
 				 case 'type':
-					$it = $k;
 					$parts = FALSE; //type field won't be displayed
 					break 2;
 				 case 'item_id':
-					$ii = $k;
 					break;
 				 case 'slotlen':
-					$is[] = $k;
+					$summers[] = $t2;
 					break;
 				}
-				if (array_key_exists($val,$translates)) {
+				if ($val[0] == 'M') {
+					unset($parts[$k]);
+				} elseif (array_key_exists($val,$translates)) {
 					$val = $translates[$val];
 				}
 			}
@@ -122,34 +112,40 @@ if ($data) {
 			if ($parts) {
 				$coltitles[] = implode('<br />',$parts);
 			}
-			$k++;
 		}
-		$coltitles[] = $this->Lang('total').'<br />'.$t; //for locally-calculated item-total booked time
 
 		$theme = ($this->before20) ? cmsms()->get_variable('admintheme'):
 			cms_utils::get_theme_object();
-		$icon_view = $theme->DisplayImage('icons/system/view.gif', $this->Lang('view'), '', '', 'systemicon');
+		$t2 = $this->Lang('tip_seetype',$this->Lang('item'));
+		$icon_view = $theme->DisplayImage('icons/system/view.gif',$t2,'','','systemicon');
 		$translates = $db->GetAssoc('SELECT item_id,name FROM '.$this->ItemTable.' ORDER BY item_id');
 		//CHECKME fallback for any missing name ?
+
 		$ic = count($pivoted);
-		$ic--; //DEBUG = skip the dodgy totals row
 		for ($i = 0; $i < $ic; $i++) {
-			$oneset = new stdClass();
-			$row = array_values($pivoted[$i]);
-			$more = ($row[$it] == Booker\PivotBase::TYPE_LINE);
-			unset($row[$it]);
-			$iid = $row[$ii];
-			$row[$ii] = $translates[$iid]; //item name
-			//interpret *\'slotlen'
-			$sum = 0.0;
-			foreach ($is as $k) {
-				$row[$k] = round(($row[$k]/$slen),1);
-				$sum += $row[$k];
+			$row = $pivoted[$i];
+			$dataline = ($row['type'] == Booker\PivotBase::TYPE_LINE);
+			unset($row['type']);
+			$iid = $row['item_id'];
+			if ($dataline) {
+				$current = $translates[$iid]; //item name
+				$row['item_id'] = $current;
+				$row['month'] = $months[$row['month']];;
+			} elseif (strpos($iid,$subtotal) !== FALSE) {
+				$row['item_id'] = str_replace('item_id',$current,$iid);
 			}
-			$row[] = $sum;
-			$oneset->fields = $row;
-			$oneset->view = ($more) ? $this->CreateLink($id,'filter','',$icon_view,
-				array('item_id'=>$iid)) : NULL; //TODO $params[]
+			//interpret *\'slotlen'
+			foreach ($summers as $t2) {
+				if (isset($row[$t2])) {
+					$a = round(($row[$t2]/$slen),1);
+					$row[$t2] = $a;
+				}
+			}
+
+			$oneset = new stdClass();
+			$oneset->fields = array_values($row);
+			$oneset->view = ($dataline) ? $this->CreateLink($id,$params['action'],'',$icon_view,
+				array('filter'=>1,'item_id'=>$iid)) : NULL; //TODO $params[]
 			$display[] = $oneset;
 		}
 	}
