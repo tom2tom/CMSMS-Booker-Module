e2cb562d78ea7e1460c0d622488a376e1ad42275
tom2tom <tpgww@onepost.net>
2017-06-23 23:02:50 +1000

    conform refactored table

--- lib/class.Cleanold_task.php
+++ lib/class.Cleanold_task.php
@@ -25,7 +25,8 @@ class Cleanold_task implements \CmsRegularTask
 	public function test($time='')
 	{
 		$mod = \cms_utils::get_module(self::MODNAME);
-		$sql = 'SELECT 1 AS gone FROM '.$mod->DataTable.' WHERE active=0 UNION SELECT 1 AS gone FROM '.$mod->RepeatTable.' WHERE active=0';
+		//TODO possible DispTable::displayed instead of Once/Repeat? 
+		$sql = 'SELECT 1 AS gone FROM '.$mod->OnceTable.' WHERE active=0 UNION SELECT 1 AS gone FROM '.$mod->RepeatTable.' WHERE active=0';
 		$res = $mod->dbHandle->GetOne($sql);
 		return ($res != FALSE);
 	}
@@ -34,13 +35,13 @@ class Cleanold_task implements \CmsRegularTask
 	{
 		$mod = \cms_utils::get_module(self::MODNAME);
 		$sql = <<<EOS
-SELECT D.bkg_id,I.item_id,I.timezone FROM $mod->DataTable D
-JOIN $mod->ItemTable I ON D.item_id=I.item_id
-WHERE D.active=0
+SELECT O.bkg_id,I.item_id,I.timezone FROM $mod->OnceTable O
+JOIN $mod->ItemTable I ON O.item_id=I.item_id
+WHERE O.active=0
 EOS;
 		$rows = $mod->dbHandle->GetArray($sql);
 		if ($rows) {
-			$sql = 'DELETE FROM '.$mod->DataTable.' WHERE bkg_id=';
+			$sql = 'DELETE FROM '.$mod->OnceTable.' WHERE bkg_id=';
 			$utils = new Utils();
 			foreach ($rows as $one) {
 				$st = $utils->GetZoneTime($one['timezone']);
@@ -51,8 +52,8 @@ EOS;
 				}
 			}
 		}
-		//TODO also $mod->RepeatTable, && maybe $mod->DataTable rows for that bulk_id
-		//TODO also $mod->XdataTable
+		//TODO also $mod->RepeatTable
+		//TODO also consequent $mod->DispTable records
 		return TRUE;
 	}
 
