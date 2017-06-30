d81a506302e1dbd6a69321982eed0682f49ee0ea
tom2tom <tpgww@onepost.net>
2017-06-30 13:49:11 +1000

    extra action

---
+++ action.bookeramounts.php
@@ -0,0 +1,8 @@
+<?php
+#----------------------------------------------------------------------
+# Module: Booker - a resource booking module
+# Action: bookeramounts - view or edit booker payments & credit
+#----------------------------------------------------------------------
+# See file Booker.module.php for full details of copyright, licence, etc.
+#----------------------------------------------------------------------
+
