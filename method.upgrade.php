546510392d947261d84726a35aafb01504d9e96f
tom2tom <tpgww@onepost.net>
2017-07-22 10:20:50 +1000

    real update code

--- method.upgrade.php
+++ method.upgrade.php
@@ -6,13 +6,34 @@
 # See file Booker.module.php for full details of copyright, licence, etc.
 #----------------------------------------------------------------------
 
-if (!$this->_CheckAccess('admin')) return;
+if (!$this->_CheckAccess('admin')) {
+	return;
+}
+
+function name_rehash(&$mod, &$cfuncs)
+{
+	global $db;
+	$sql = 'SELECT booker_id,name FROM '.$mod->BookerTable.' WHERE auth_id=0';
+	$rst = $db->Execute($sql);
+	if ($rst) {
+		$sql = 'UPDATE '.$mod->BookerTable.' SET namehash=? WHERE booker_id=?';
+		$pw = $cfuncs->decrypt_preference(Booker\Crypter::MKEY);
+		while (!$rst->EOF) {
+			$name = $cfuncs->uncloak_value($rst->fields['name'], $pw);
+			$hash = ($name) ? $cfuncs->hash_value($name, $pw) : NULL;
+			$db->Execute($sql, [$hash, $rst->fields['booker_id']]);
+			if (!$rst->MoveNext()) {
+				break;
+			}
+		}
+		$rst->Close();
+	}
+}
 
 $dict = NewDataDictionary($db);
-$pref = \cms_db_prefix();
 
 switch ($oldversion) {
-case '0.6':
+/* case '0.6':
 	$t = 'nQCeESKBr99A';
 	$this->SetPreference($t, hash('sha256', $t.microtime()));
 	$pw = $this->GetPreference('masterpass');
@@ -21,83 +42,142 @@ case '0.6':
 		$pw = substr($s,5);
 	}
 	$cfuncs = new Booker\Crypter($this);
-	$cfuncs->encrypt_preference('masterpass',$pw);
-
-/*
-	$dict = NewDataDictionary($db);
-
-//	$sqlarray = $dict->AddColumnSQL($this->BookerTable, 'auth_id I(4) DEFAULT 0');
-//	$dict->ExecuteSqlArray($sqlarray);
-//	$sqlarray = ['ALTER TABLE '.$this->BookerTable.' CHANGE auth_id auth_id int(4) NULL AFTER booker_id'];
-//	$dict->ExecuteSqlArray($sqlarray,FALSE);
-//	$sqlarray = $dict->AddColumnSQL($this->BookerTable, 'namehash B');
-//	$dict->ExecuteSqlArray($sqlarray);
-//	$sqlarray = ['ALTER TABLE '.$this->BookerTable.' CHANGE namehash namehash longblob NULL AFTER auth_id'];
-//	$dict->ExecuteSqlArray($sqlarray,FALSE);
+	$cfuncs->encrypt_preference('masterpass');
+*/
+case '0.6':
 	$sqlarray = $dict->AlterColumnSQL($this->BookerTable, 'name B');
+	$dict->ExecuteSqlArray($sqlarray);
+	$sqlarray = $dict->AddColumnSQL($this->BookerTable, 'auth_id I(4) DEFAULT 0');
+	$dict->ExecuteSqlArray($sqlarray);
+	$sqlarray = $dict->AddColumnSQL($this->BookerTable, 'namehash B');
 	$dict->ExecuteSqlArray($sqlarray);
 
 	$cfuncs = new Booker\Crypter($this);
-	$mpw = $this->GetPreference('masterpass');
-	if ($mpw) {
-		$mpw = $cfuncs->olddecrypt_preference('masterpass');
-		$cfuncs->encrypt_preference('masterpass',$mpw);
-		$this->RemovePreference('masterpass');
-	} else {
-		$mpw = $cfuncs->decrypt_preference('masterpass');
-		if (!$mpw) {
-			$mpw = 'Suck it up, crackers!';
-			$cfuncs->encrypt_preference('masterpass',$mpw);
+	$t = $this->GetPreference('nQCeESKBr99A');
+	if ($t) {
+		$val = hash('crc32b', $t.$config['ssl_url'].$this->GetModulePath());
+		$this->RemovePreference('nQCeESKBr99A');
+
+		$key = 'masterpass';
+		$s = base64_decode($this->GetPreference($key));
+		$pw = $cfuncs->decrypt($s, $val);
+		if (!$pw) {
+			$pw = base64_decode('V09PIEhPTyB0aGlzIGlzIE5FVw==');
 		}
+		$this->RemovePreference($key);
+		$cfuncs->init_crypt();
+		$cfuncs->encrypt_preference(Booker\Crypter::MKEY, $pw);
+
+/*		foreach ([] as $key) {
+			$s = base64_decode($this->GetPreference($key));
+			$t = $cfuncs->decrypt($s, $val);
+			$this->RemovePreference($key);
+			$cfuncs->encrypt_preference($key, $t);
+		}
+*/
+	} else {
+		$pw = $cfuncs->decrypt_preference(Booker\Crypter::MKEY);
 	}
 
-	$sql = 'SELECT booker_id,publicid,name FROM '.$this->BookerTable;
+	$sql = 'SELECT publicid FROM '.$this->BookerTable;
 	$rst = $db->Execute($sql);
-	if ($rst) {
-		$afuncs = new Auther\Auth(NULL,$this->GetPreference('authcontext',0));
-		if ($afuncs) {
-			$sql1 = 'UPDATE '.$this->BookerTable.' SET auth_id=?,namehash=NULL,name=NULL WHERE booker_id=?';
-			$sql2 = 'DELETE FROM '.$this->BookerTable.' WHERE booker_id=?';
-			$sql3 = 'UPDATE '.$this->BookerTable.' SET auth_id=0,namehash=?,name=? WHERE booker_id=?';
+	if ($rst && !$rst->EOF) {
+		$rst->Close();
+		$sql = 'SELECT booker_id,publicid,name,address,phone FROM '.$this->BookerTable;
+		$rst = $db->Execute($sql);
+		if ($rst) {
+			$afuncs = new Auther\Auth(NULL, $this->GetPreference('authcontext', 0));
+			if ($afuncs) {
+				$sql1 = 'UPDATE '.$this->BookerTable.' SET auth_id=?,namehash=NULL,name=NULL WHERE booker_id=?';
+				$sql2 = 'DELETE FROM '.$this->BookerTable.' WHERE booker_id=?';
+				$sql3 = 'UPDATE '.$this->BookerTable.' SET auth_id=0,namehash=?,name=?,address=?,phone=? WHERE booker_id=?';
 
-			while (!$rst->EOF) {
-				if ($rst->fields['publicid']) {
-					$aid = $afuncs->GetUserID($rst->fields['publicid']);
-					if ($aid) {
-						$db->Execute($sql1,[$aid,$rst->fields['booker_id']]);
+				while (!$rst->EOF) {
+					if ($rst->fields['publicid']) {
+						$aid = $afuncs->GetUserID($rst->fields['publicid']);
+						if ($aid) {
+							$db->Execute($sql1, [$aid, $rst->fields['booker_id']]);
+						} else {
+							$db->Execute($sql2, [$rst->fields['booker_id']]);
+						}
 					} else {
-						$db->Execute($sql2,[$rst->fields['booker_id']]);
+						$t = $rst->fields['name'];
+						if ($t) {
+							$name = $cfuncs->decrypt_value($t, $pw);
+							if ($name) {
+								$t = $name;
+							}
+						}
+						$hash = ($t) ? $cfuncs->hash_value($t, $pw) : NULL;
+						$name = ($t) ? $cfuncs->cloak_value($t, 0, $pw) : NULL;
+						$t = $rst->fields['address'];
+						if ($t) {
+							$address = $cfuncs->decrypt_value($t, $pw);
+							if ($address) {
+								$t = $address;
+							}
+						}
+						$address = ($t) ? $cfuncs->cloak_value($t, 0, $pw) : NULL;
+						$t = $rst->fields['phone'];
+						if ($t) {
+							$phone = $cfuncs->decrypt_value($t, $pw);
+							if ($phone) {
+								$t = $phone;
+							}
+						}
+						$phone = ($t) ? $cfuncs->cloak_value($t, 16, $pw) : NULL;
+
+						$db->Execute($sql3, [$hash, $name, $address, $phone, $rst->fields['booker_id']]);
 					}
-				} else {
-					$hash = $cfuncs->hash_value($rst->fields['name'],$mpw);
-					$name = $cfuncs->encrypt_value($rst->fields['name'],$mpw);
-					$db->Execute($sql3,[$hash,$name,$rst->fields['booker_id']]);
+					if (!$rst->MoveNext()) {
+						break;
+					}
 				}
+			}
+			$rst->Close();
+		}
+		$sqlarray = $dict->DropColumnSQL($this->BookerTable, 'publicid');
+		$dict->ExecuteSqlArray($sqlarray);
+	} else {
+		$sql = 'SELECT booker_id,name,address,phone FROM '.$this->BookerTable.' WHERE auth_id=0';
+		$rst = $db->Execute($sql);
+		if ($rst) {
+			$sql3 = 'UPDATE '.$this->BookerTable.' SET name=?,address=?,phone=? WHERE booker_id=?';
+			while (!$rst->EOF) {
+				$t = $rst->fields['name'];
+				if ($t) {
+					$name = $cfuncs->decrypt_value($t, $pw);
+					if ($name) {
+						$t = $name;
+					}
+				}
+				$name = ($t) ? $cfuncs->cloak_value($t, 0, $pw) : NULL;
+				$t = $rst->fields['address'];
+				if ($t) {
+					$address = $cfuncs->decrypt_value($t, $pw);
+					if ($address) {
+						$t = $address;
+					}
+				}
+				$address = ($t) ? $cfuncs->cloak_value($t, 0, $pw) : NULL;
+				$t = $rst->fields['phone'];
+				if ($t) {
+					$phone = $cfuncs->decrypt_value($t, $pw);
+					if ($phone) {
+						$t = $phone;
+					}
+				}
+				$phone = ($t) ? $cfuncs->cloak_value($t, 16, $pw) : NULL;
+
+				$db->Execute($sql3, [$name, $address, $phone, $rst->fields['booker_id']]);
+
 				if (!$rst->MoveNext()) {
 					break;
 				}
 			}
+			$rst->Close();
 		}
-		$rst->Close();
 	}
-
-	$sqlarray = $dict->DropColumnSQL($this->BookerTable, 'publicid');
-	$dict->ExecuteSqlArray($sqlarray);
-*/
-/*
-$sql = <<<EOS
-SELECT booker_id,name FROM $this->BookerTable WHERE name IS NOT NULL
-EOS;
-$data = $db->GetArray($sql);
-$sql = <<<EOS
-UPDATE $this->BookerTable SET namehash=? WHERE booker_id=?
-EOS;
-$cfuncs = new Booker\Crypter($this);
-$mpw = $cfuncs->decrypt_preference('masterpass');
-foreach ($data as $one) {
-	$name = $cfuncs->decrypt_value($one['name'], $mpw);
-	$hash = $cfuncs->hash_value($name, $mpw);
-	$db->Execute($sql,[$hash,$one['booker_id']]);
-}
-*/
+// case '':
+//	name_rehash($this,$cfuncs);
 }
