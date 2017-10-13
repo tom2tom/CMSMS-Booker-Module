e3543ea077fcc5d8ed3df23841d7c3d190e796bc
tom2tom <tpgww@onepost.net>
2017-10-13 19:03:51 +1100

    use custom datetime inputs

--- action.processamounts.php
+++ action.processamounts.php
@@ -484,12 +484,10 @@ EOS;
 
 $tplvars['title_range'] = $this->Lang('title_report_change');
 $tplvars['titlefrom'] = $this->Lang('start');
-$t = $this->CreateInputText($id,'showfrom','',12,15);
-$tplvars['showfrom'] = str_replace('class="','class="dateinput ',$t);
+$tplvars['showfrom'] = $this->_CreateInputDate($id,'showfrom','',12,15);
 $tplvars['helpfrom'] = $this->Lang('help_reportfrom');
 $tplvars['titleto'] = $this->Lang('end');
-$t = $this->CreateInputText($id,'showto','',12,15);
-$tplvars['showto'] = str_replace('class="','class="dateinput ',$t);
+$tplvars['showto'] = $this->_CreateInputDate($id,'showto','',12,15);
 $tplvars['helpto'] = $this->Lang('help_reportto');
 $tplvars['range'] = $this->CreateInputSubmit($id,'range',$this->Lang('apply'),
 	'title="'.$this->Lang('tip_report_interval').'"');
@@ -512,20 +510,25 @@ $jsloads[] = <<<EOS
   longMonths: [$mnames],
   shortMonths: [$smnames],
  });
- $('.dateinput').pikamonth({
-  format: 'Y-m',
-  reformat: function(target,f) {
-   return fmt.formatDate(target,f);
-  },
-  getdate: function(target,f) {
-   return fmt.parseDate(target,f);
-  },
-  i18n: {
-   previousYear: '$prevyr',
-   nextYear: '$nextyr',
-   months: [$mnames],
-   monthsShort: [$smnames]
-  }
+ $('.dateinput_container img').each(function() {
+  var \$th=$(this),
+   \$tg=\$th.parent().find('.dateinput');
+  \$th.pikamonth({
+   field: \$tg[0],
+   format: 'Y-m',
+   reformat: function(target,f) {
+    return fmt.formatDate(target,f);
+   },
+   getdate: function(target,f) {
+    return fmt.parseDate(target,f);
+   },
+   i18n: {
+    previousYear: '$prevyr',
+    nextYear: '$nextyr',
+    months: [$mnames],
+    monthsShort: [$smnames]
+   }
+  });
  });
 EOS;
 
